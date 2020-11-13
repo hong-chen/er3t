9ef7388757afcf0431b8e8621947ce858d70d2f7
hong-chen <me@hongchen.cz>
2020-11-13 13:47:45 -0700

    add a check program for future after-installation check

---
+++ tests/check.py
@@ -0,0 +1,414 @@
+import os
+import sys
+import pickle
+import h5py
+from pyhdf.SD import SD, SDC
+import numpy as np
+import datetime
+import time
+from scipy.io import readsav
+from scipy import interpolate
+from scipy import stats
+from scipy.optimize import curve_fit
+import matplotlib as mpl
+import matplotlib.image as mpl_img
+# mpl.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator
+from matplotlib import rcParams
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
+
+from er3t.pre.atm import atm_atmmod
+from er3t.pre.abs import abs_16g
+from er3t.pre.cld import cld_sat
+from er3t.pre.sfc import sfc_sat
+from er3t.util.modis import modis_l1b, modis_l2, modis_03, modis_09a1, grid_modis_by_extent, grid_modis_by_lonlat, download_modis_https, get_sinusoidal_grid_tag, get_filename_tag, download_modis_rgb
+from er3t.util import cal_r_twostream
+
+from er3t.rtm.mca_v010 import mca_atm_1d, mca_atm_3d, mca_sfc_2d
+from er3t.rtm.mca_v010 import mcarats_ng
+from er3t.rtm.mca_v010 import mca_out_ng
+
+
+
+class satellite_download:
+
+    def __init__(
+            self,
+            date=None,
+            extent=None,
+            fname=None,
+            fdir_out='data',
+            overwrite=False,
+            quiet=False,
+            verbose=False):
+
+        self.date     = date
+        self.extent   = extent
+        self.fdir_out = fdir_out
+        self.quiet    = quiet
+        self.verbose  = verbose
+
+        if ((fname is not None) and (os.path.exists(fname)) and (not overwrite)):
+
+            self.load(fname)
+
+        elif (((date is not None) and (extent is not None)) and (fname is not None) and (os.path.exists(fname)) and (overwrite)) or \
+             (((date is not None) and (extent is not None)) and (fname is not None) and (not os.path.exists(fname))):
+
+            self.run()
+            self.dump(fname)
+
+        elif (((date is not None) and (extent is not None)) and (fname is None)):
+
+            self.run()
+
+        else:
+
+            sys.exit('Error   [satellite]: Please check if \'%s\' exists or provide \'date\' and \'extent\' to proceed.' % fname)
+
+    def load(self, fname):
+
+        with open(fname, 'rb') as f:
+            obj = pickle.load(f)
+            if hasattr(obj, 'fnames') and hasattr(obj, 'extent') and hasattr(obj, 'fdir_out') and hasattr(obj, 'date'):
+                if self.verbose:
+                    print('Message [satellite]: Loading %s ...' % fname)
+                self.date     = obj.date
+                self.extent   = obj.extent
+                self.fnames   = obj.fnames
+                self.fdir_out = obj.fdir_out
+            else:
+                sys.exit('Error   [satellite]: File \'%s\' is not the correct pickle file to load.' % fname)
+
+    def run(self, run=True):
+
+        lon = np.array(self.extent[:2])
+        lat = np.array(self.extent[2:])
+
+        self.fnames = {}
+
+        self.fnames['mod_rgb'] = [download_modis_rgb(self.date, self.extent, fdir=self.fdir_out, which='aqua', coastline=True)]
+
+        # plot
+        # =================================================================================
+        rcParams['font.size'] = 12
+
+        fig = plt.figure(figsize=(6, 5))
+        ax1 = fig.add_subplot(111)
+        img = mpl_img.imread(self.fnames['mod_rgb'][0])
+        cs = ax1.imshow(img, extent=self.extent, zorder=0)
+        ax1.set_xlabel('Longitude [$^\circ$]')
+        ax1.set_ylabel('Latitude [$^\circ$]')
+        ax1.set_xlim(self.extent[:2])
+        ax1.set_ylim(self.extent[2:])
+        ax1.set_title('Here\'s the MODIS RGB Imagery for \nthe region and date you specified\n(close this window to proceed)', y=1.0)
+        plt.show()
+        # ------------------------------------------------------------------------------------------------------
+
+
+        # MODIS Level 2 Cloud Product and MODIS 03 geo file
+        self.fnames['mod_l2'] = []
+        self.fnames['mod_02_1km'] = []
+        self.fnames['mod_02'] = []
+        self.fnames['mod_03'] = []
+        filename_tags_03 = get_filename_tag(self.date, lon, lat, satID='aqua')
+        for filename_tag in filename_tags_03:
+            fnames_l2 = download_modis_https(self.date, '61/MYD06_L2', filename_tag, day_interval=1, fdir_out=self.fdir_out, run=run)
+            fnames_02_1km = download_modis_https(self.date, '61/MYD021KM', filename_tag, day_interval=1, fdir_out=self.fdir_out, run=run)
+            fnames_02 = download_modis_https(self.date, '61/MYD02QKM', filename_tag, day_interval=1, fdir_out=self.fdir_out, run=run)
+            fnames_03 = download_modis_https(self.date, '61/MYD03'   , filename_tag, day_interval=1, fdir_out=self.fdir_out, run=run)
+            self.fnames['mod_l2'] += fnames_l2
+            self.fnames['mod_02_1km'] += fnames_02_1km
+            self.fnames['mod_02'] += fnames_02
+            self.fnames['mod_03'] += fnames_03
+
+        # MOD09A1 surface reflectance product
+        self.fnames['mod_09'] = []
+        filename_tags_09 = get_sinusoidal_grid_tag(lon, lat)
+        for filename_tag in filename_tags_09:
+            fnames_09 = download_modis_https(self.date, '6/MOD09A1', filename_tag, day_interval=8, fdir_out=self.fdir_out, run=run)
+            self.fnames['mod_09'] += fnames_09
+
+    def dump(self, fname):
+
+        self.fname = fname
+        with open(fname, 'wb') as f:
+            if self.verbose:
+                print('Message [satellite]: Saving object into %s ...' % fname)
+            pickle.dump(self, f)
+
+
+def para_corr(lon0, lat0, vza, vaa, cld_h, sfc_h, R_earth=6378000.0, verbose=True):
+
+    """
+    Parallax correction for the cloud positions
+
+    lon0: input longitude
+    lat0: input latitude
+    vza : sensor zenith angle [degree]
+    vaa : sensor azimuth angle [degree]
+    cld_h: cloud height [meter]
+    sfc_h: surface height [meter]
+    """
+
+    if verbose:
+        print('Message [para_corr]: Please make sure the units of \'cld_h\' and \'sfc_h\' are in \'meter\'.')
+
+    dist = (cld_h-sfc_h)*np.tan(np.deg2rad(vza))
+
+    delta_lon = dist*np.sin(np.deg2rad(vaa)) / (np.pi*R_earth) * 180.0
+    delta_lat = dist*np.cos(np.deg2rad(vaa)) / (np.pi*R_earth) * 180.0
+
+    lon = lon0 + delta_lon
+    lat = lat0 + delta_lat
+
+    return lon, lat
+
+def pre_cld(sat, cth=None, scale_factor=1.0, solver='3D'):
+
+    # retrieve 1. cloud top height; 2. sensor zenith; 3. sensor azimuth for MODIS L1B (250nm) data from MODIS L2 (5km resolution)
+    # ===================================================================================
+    modl2      = modis_l2(fnames=sat.fnames['mod_l2'], extent=sat.extent, vnames=['Sensor_Zenith', 'Sensor_Azimuth', 'Cloud_Top_Height'])
+    logic_cth  = (modl2.data['cloud_top_height']['data']>0.0)
+    lon0       = modl2.data['lon_5km']['data']
+    lat0       = modl2.data['lat_5km']['data']
+    cth0       = modl2.data['cloud_top_height']['data']
+
+    mod03      = modis_03(fnames=sat.fnames['mod_03'], extent=sat.extent, vnames=['Height'])
+    logic_sfh  = (mod03.data['height']['data']>0.0)
+    lon1       = mod03.data['lon']['data']
+    lat1       = mod03.data['lat']['data']
+    sfh1       = mod03.data['height']['data']
+    sza1       = mod03.data['sza']['data']
+    saa1       = mod03.data['saa']['data']
+    vza1       = mod03.data['vza']['data']
+    vaa1       = mod03.data['vaa']['data']
+
+    modl1b = modis_l1b(fnames=sat.fnames['mod_02'], extent=sat.extent)
+    lon_2d, lat_2d, ref_2d = grid_modis_by_extent(modl1b.data['lon']['data'], modl1b.data['lat']['data'], modl1b.data['ref']['data'][0, ...], extent=sat.extent)
+    lon_2d, lat_2d, rad_2d = grid_modis_by_extent(modl1b.data['lon']['data'], modl1b.data['lat']['data'], modl1b.data['rad']['data'][0, ...], extent=sat.extent)
+
+    a0         = np.median(ref_2d)
+    mu0        = np.cos(np.deg2rad(sza1.mean()))
+
+    xx_2stream = np.linspace(0.0, 200.0, 10000)
+    yy_2stream = cal_r_twostream(xx_2stream, a=a0, mu=mu0)
+
+    threshold  = a0 * 2.0
+    indices    = np.where(ref_2d>threshold)
+    indices_x  = indices[0]
+    indices_y  = indices[1]
+    lon        = lon_2d[indices_x, indices_y]
+    lat        = lat_2d[indices_x, indices_y]
+
+    # parallax correction
+    # ====================================================================================================
+    if cth is None:
+        points     = np.transpose(np.vstack((lon0[logic_cth], lat0[logic_cth])))
+        cth        = interpolate.griddata(points, cth0[logic_cth], (lon, lat), method='nearest')
+
+    points     = np.transpose(np.vstack((lon1[logic_sfh], lat1[logic_sfh])))
+    sfh        = interpolate.griddata(points, sfh1[logic_sfh], (lon, lat), method='nearest')
+
+    points     = np.transpose(np.vstack((lon1, lat1)))
+    vza        = interpolate.griddata(points, vza1, (lon, lat), method='nearest')
+    vaa        = interpolate.griddata(points, vaa1, (lon, lat), method='nearest')
+
+    if solver == '3D':
+        lon_corr, lat_corr = para_corr(lon, lat, vza, vaa, cth*1000.0, sfh)
+    elif solver == 'IPA':
+        lon_corr, lat_corr = para_corr(lon, lat, vza, vaa, sfh, sfh)
+    # ====================================================================================================
+
+    lon_1d = lon_2d[:, 0]
+    indices_x_new = np.int_(np.round((lon_corr-lon_1d[0])/(((lon_1d[1:]-lon_1d[:-1])).mean()), decimals=0))
+    lat_1d = lat_2d[0, :]
+    indices_y_new = np.int_(np.round((lat_corr-lat_1d[0])/(((lat_1d[1:]-lat_1d[:-1])).mean()), decimals=0))
+
+    Nx, Ny = ref_2d.shape
+    cot_2d_l1b = np.zeros_like(ref_2d)
+    cer_2d_l1b = np.zeros_like(ref_2d); cer_2d_l1b[...] = 1.0
+    for i in range(indices_x.size):
+        if 0<=indices_x_new[i]<Nx and 0<=indices_y_new[i]<Ny:
+            cot_2d_l1b[indices_x_new[i], indices_y_new[i]] = xx_2stream[np.argmin(np.abs(yy_2stream-ref_2d[indices_x[i], indices_y[i]]))]
+            cer_2d_l1b[indices_x_new[i], indices_y_new[i]] = 12.0
+
+    modl1b.data['lon_2d'] = dict(name='Gridded longitude'               , units='degrees'    , data=lon_2d)
+    modl1b.data['lat_2d'] = dict(name='Gridded latitude'                , units='degrees'    , data=lat_2d)
+    modl1b.data['rad_2d'] = dict(name='Gridded radiance'                , units='W/m^2/nm/sr', data=rad_2d)
+    modl1b.data['ref_2d'] = dict(name='Gridded reflectance'             , units='N/A'        , data=ref_2d)
+    modl1b.data['cot_2d'] = dict(name='Gridded cloud optical thickness' , units='N/A'        , data=cot_2d_l1b*scale_factor)
+    modl1b.data['cer_2d'] = dict(name='Gridded cloud effective radius'  , units='micro'      , data=cer_2d_l1b)
+
+    return modl1b
+
+def pre_sfc(sat):
+
+    # band 1: 620  - 670  nm, index 0
+    # band 2: 841  - 876  nm, index 1
+    # band 3: 459  - 479  nm, index 2
+    # band 4: 545  - 565  nm, index 3
+    # band 5: 1230 - 1250 nm, index 4
+    # band 6: 1628 - 1652 nm, index 5
+    # band 7: 2105 - 2155 nm, index 6
+    mod = modis_09a1(fnames=sat.fnames['mod_09'], extent=sat.extent)
+
+    lon_2d, lat_2d, mod_sfc_alb_08_2d = grid_modis_by_extent(mod.data['lon']['data'], mod.data['lat']['data'], mod.data['ref']['data'][0, :], extent=sat.extent)
+
+    mod.data['alb_2d'] = dict(data=mod_sfc_alb_08_2d, name='Surface albedo', units='N/A')
+    mod.data['lon_2d'] = dict(data=lon_2d, name='Longitude', units='degrees')
+    mod.data['lat_2d'] = dict(data=lat_2d, name='Latitude' , units='degrees')
+
+    return mod
+
+def cal_mca_rad_modis(date, sat, wavelength, cth=None, scale_factor=1.0, fdir='tmp-data', solver='3D', overwrite=True):
+
+    """
+    Calculate OCO2 radiance using cloud (MODIS level 1b) and surface properties (MOD09A1) from MODIS
+    """
+
+    # atm object
+    # =================================================================================
+    levels    = np.array([ 0. ,  0.5,  1. ,  1.5,  2. ,  2.5,  3. ,  3.5,  4. ,  4.5,  5. , 5.5,  6. ,  6.5,  7. ,  7.5,  8. ,  8.5,  9. ,  9.5, 10. , 11. , 12. , 13. , 14. , 20. , 25. , 30. , 35. , 40. ])
+    fname_atm = '%s/atm.pk' % fdir
+    atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
+    # =================================================================================
+
+    # abs object, in the future, we will implement OCO2 MET file for this
+    # =================================================================================
+    fname_abs = '%s/abs.pk' % fdir
+    abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
+    # =================================================================================
+
+    # mca_sfc object
+    # =================================================================================
+    mod09 = modis_09a1(fnames=sat.fnames['mod_09'], extent=sat.extent)
+    lon_2d, lat_2d, surf_ref_2d = grid_modis_by_extent(mod09.data['lon']['data'], mod09.data['lat']['data'], mod09.data['ref']['data'][0, ...], extent=sat.extent)
+    mod09.data['alb_2d'] = dict(data=surf_ref_2d, name='Surface albedo', units='N/A')
+    mod09.data['lon_2d'] = dict(data=lon_2d, name='Longitude', units='degrees')
+    mod09.data['lat_2d'] = dict(data=lat_2d, name='Latitude' , units='degrees')
+
+    fname_sfc = '%s/sfc.pk' % fdir
+    sfc0      = sfc_sat(sat_obj=mod09, fname=fname_sfc, extent=sat.extent, verbose=True, overwrite=overwrite)
+    sfc_2d    = mca_sfc_2d(atm_obj=atm0, sfc_obj=sfc0, fname='%s/mca_sfc_2d.bin' % fdir, overwrite=overwrite)
+    # =================================================================================
+
+    # cld object
+    # =================================================================================
+    modl1b    = pre_cld(sat, cth=cth, scale_factor=scale_factor, solver=solver)
+    fname_cld = '%s/cld.pk' % fdir
+    cld0      = cld_sat(sat_obj=modl1b, fname=fname_cld, cth=cth, cgt=1.0, dz=np.unique(atm0.lay['thickness']['data'])[0], overwrite=overwrite)
+
+    atm3d0  = mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir)
+    atm1d0  = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+    atm_1ds = [atm1d0]
+    atm_3ds = [atm3d0]
+    # =================================================================================
+
+    # modis 03 parameters
+    # =================================================================================
+    mod03   = modis_03(fnames=sat.fnames['mod_03'], extent=sat.extent)
+    sza     = mod03.data['sza']['data'].mean()
+    saa     = mod03.data['saa']['data'].mean()
+    vza     = mod03.data['vza']['data'].mean()
+    vaa     = mod03.data['vaa']['data'].mean()
+    # =================================================================================
+
+    # run mcarats
+    # =================================================================================
+    mca0 = mcarats_ng(
+            date=date,
+            atm_1ds=atm_1ds,
+            atm_3ds=atm_3ds,
+            sfc_2d=sfc_2d,
+            Ng=abs0.Ng,
+            target='radiance',
+            solar_zenith_angle   = sza,
+            solar_azimuth_angle  = saa,
+            sensor_zenith_angle  = vza,
+            sensor_azimuth_angle = vaa,
+            fdir='%s/%.4fnm/oco2/rad_%s' % (fdir, wavelength, solver.lower()),
+            Nrun=3,
+            weights=abs0.coef['weight']['data'],
+            photons=1e7,
+            solver=solver,
+            Ncpu=5,
+            mp_mode='py',
+            overwrite=overwrite
+            )
+    # =================================================================================
+
+    # mcarats output
+    # =================================================================================
+    out0 = mca_out_ng(fname='%s/mca-out-rad-oco2-%s_%.4fnm.h5' % (fdir, solver.lower(), wavelength), mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
+    # =================================================================================
+
+    # plot
+    # =================================================================================
+    rcParams['font.size'] = 12
+
+    fig = plt.figure(figsize=(16, 5))
+    ax1 = fig.add_subplot(131)
+    ax2 = fig.add_subplot(132)
+    ax3 = fig.add_subplot(133)
+
+    img = mpl_img.imread(sat.fnames['mod_rgb'][0])
+    cs = ax1.imshow(img, extent=sat.extent, zorder=0)
+    ax1.set_xlabel('Longitude [$^\circ$]')
+    ax1.set_ylabel('Latitude [$^\circ$]')
+    ax1.set_xlim(sat.extent[:2])
+    ax1.set_ylim(sat.extent[2:])
+    ax1.set_title('MODIS RGB Imagery')
+
+    cs = ax2.imshow(modl1b.data['rad_2d']['data'].T, cmap='Greys_r', origin='lower', vmin=0.0, vmax=0.3, extent=sat.extent, zorder=0)
+    ax2.set_xlabel('Longitude [$^\circ$]')
+    ax2.set_ylabel('Latitude [$^\circ$]')
+    ax2.set_xlim(sat.extent[:2])
+    ax2.set_ylim(sat.extent[2:])
+    ax2.set_title('MODIS Band 1 (650nm)')
+
+    cs = ax3.imshow(out0.data['rad']['data'].T, cmap='Greys_r', origin='lower', vmin=0.0, vmax=0.3, extent=sat.extent, zorder=0)
+    ax3.set_xlabel('Longitude [$^\circ$]')
+    ax3.set_ylabel('Latitude [$^\circ$]')
+    ax3.set_xlim(sat.extent[:2])
+    ax3.set_ylim(sat.extent[2:])
+    ax3.set_title('Simulated Radiance at 650 nm (3D)')
+    plt.subplots_adjust(wspace=0.4)
+    plt.show()
+    # ------------------------------------------------------------------------------------------------------
+
+
+def main(date, region):
+
+    date_s = date.strftime('%Y%m%d')
+
+    # create data/YYYYMMDD directory if it does not exist
+    fdir_data = os.path.abspath('data/%s' % date_s)
+    if not os.path.exists(fdir_data):
+        os.makedirs(fdir_data)
+
+    # download satellite data
+    fname_sat = '%s/sat_%s.pk' % (fdir_data, date_s)
+    sat0 = satellite_download(date=date, fdir_out=fdir_data, extent=region, fname=fname_sat, overwrite=False)
+
+    # create tmp-data directory if it does not exist
+    fdir_tmp = os.path.abspath('tmp-data/%s' % date_s)
+    if not os.path.exists(fdir_tmp):
+        os.makedirs(fdir_tmp)
+
+    # run calculations
+    for wavelength in [650.0]:
+        for solver in ['3D']:
+            cal_mca_rad_modis(date, sat0, wavelength, cth=2.0, scale_factor=1.0, fdir=fdir_tmp, solver=solver, overwrite=True)
+
+
+
+if __name__ == '__main__':
+
+
+    date   = datetime.datetime(2017, 8, 25)
+    region = [-112.0, -111.0, 29.4, 30.4]
+
+    main(date, region)
