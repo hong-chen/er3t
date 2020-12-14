73abaca275b74313d11b99f271915c32225d7c24
hong-chen <me@hongchen.cz>
2020-12-14 14:00:31 -0700

    henyey-greenstein phase function

--- er3t/pre/pha/pha_hg.py
+++ er3t/pre/pha/pha_hg.py
@@ -1,31 +1,45 @@
 import er3t
 
-class pha_mca:
 
-    fdir = '%s/pha' % er3t.common.fdir_data
 
-    def __init__(self):
+def cal_hg_pha_func(asy, ang):
 
-        pass
+    """
+    Henyey-Greenstein phase function
 
-    def make_h_g(self):
+    Input:
+        asy: asymmetry parameter
+        ang: angles in degree (within the range of -180 to 180)
 
-        """
-        Henyey-Greenstein phase function
-        """
+    Output:
+        pha: phase function
+    """
 
-        self.value = 0.85
 
-        pass
+    mu = np.cos(np.deg2rad(ang))
+    pha = 0.5*(1.0-asy**2.0)/((1.0-2.0*asy*mu+asy**2.0)**1.5)
 
-    def make_16g(self):
+    return pha
 
-        pass
+
+
+class pha_hg:
+
+    def __init__(self, asy_params, angles):
+
+        pha = np.zeros((angles.size, asy_params.size), dtype=np.float64)
+
+        for i, asy in enumerate(asy_params):
+            pha[:, i] = cal_hg_pha_func(asy, angles)
+
+        self.data = {
+                'ang': angles,
+                'asy': asy_params,
+                'pha': pha
+                }
+
+
 
 if __name__ == '__main__':
 
-    a = pha_mca()
-    print(a.fdir)
-
     pass
-
