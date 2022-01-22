e49c7efd0befc15a1cd3daa00c66dc5a04af4809
hong-chen <hong.chen.cu@gmail.com>
2022-01-22 15:58:39 -0700

    continue work on tests/06_pre_cam.py (incomplete)

--- tests/06_pre_cam.py
+++ tests/06_pre_cam.py
@@ -45,85 +45,6 @@ from er3t.rtm.mca_v010 import mca_out_ng
 
 
 
-def run_rad_sim(f_mca, wavelength=600, fname_nc, fdir0, coarsen_factor=2, overwrite=True):
-
-    """
-    core function to run radiance simulation
-    """
-
-    fdir = '%s/%dnm' % (fdir0, wavelength)
-
-    if not os.path.exists(fdir):
-        os.makedirs(fdir)
-
-    levels = np.arange(0.0, 20.1, 1.0)
-
-    fname_atm = '%s/atm.pk' % fdir
-    atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
-    fname_abs = '%s/abs.pk' % fdir
-    abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
-
-    fname_les = '%s/les.pk' % fdir
-    cld0      = cld_les(fname_nc=fname_nc, fname=fname_les, altitude=atm0.lay['altitude']['data'], coarsing=[1, 1, 1, 1], overwrite=overwrite)
-
-    # radiance 3d
-    # =======================================================================================================
-    target    = 'radiance'
-    solver    = '3D'
-    atm1d0    = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
-    atm3d0    = mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir)
-
-    # coarsen
-    atm3d0.nml['Atm_dx']['data'] *= coarsen_factor
-    atm3d0.nml['Atm_dy']['data'] *= coarsen_factor
-
-    atm_1ds   = [atm1d0]
-    atm_3ds   = [atm3d0]
-
-    mca0 = mcarats_ng(
-            date=datetime.datetime(2016, 8, 29),
-            atm_1ds=atm_1ds,
-            atm_3ds=atm_3ds,
-            Ng=abs0.Ng,
-            target=target,
-            surface_albedo=0.0,
-            solar_zenith_angle=29.162360459281544,
-            solar_azimuth_angle=-63.16777636586792,
-            sensor_zenith_angle=0.0,
-            sensor_azimuth_angle=0.0,
-            fdir='%s/%4.4d/rad_%s' % (fdir, wavelength, solver.lower()),
-            Nrun=3,
-            # photons=1e8*coarsen_factor,
-            # photons=2e9,
-            photons=1e6,
-            weights=abs0.coef['weight']['data'],
-            solver=solver,
-            Ncpu=24,
-            mp_mode='py',
-            overwrite=overwrite)
-
-    out0 = mca_out_ng(fname='%s/mca-out-rad-%s_%.2fnm.h5' % (fdir0, solver.lower(), wavelength), mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
-    rad_3d      = out0.data['rad']['data']
-    rad_3d[np.isnan(rad_3d)] = 0.0
-    rad_3d[rad_3d<0.0] = 0.0
-
-    cot_true      = np.sum(cld0.lay['cot']['data'], axis=-1)
-    cot_true[np.isnan(cot_true)] = 0.0
-    cot_true[cot_true<0.0] = 0.0
-
-    cot_1d      = f_mca.interp_from_rad(rad_3d)
-    cot_1d[np.isnan(cot_1d)] = 0.0
-    cot_1d[cot_1d<0.0] = 0.0
-    # =======================================================================================================
-
-    fname_new = 'data/data_%s_coa-fac-%d_%dnm.h5' % (os.path.basename(fdir0), coarsen_factor, wavelength)
-    f = h5py.File(fname_new, 'w')
-
-    f['cot_true'] = cot_true
-    f['rad_3d']   = rad_3d
-    f['cot_1d']   = cot_1d
-
-    f.close()
 
 
 class aircraft:
@@ -132,9 +53,162 @@ class aircraft:
     create an aircraft object
     """
 
-    def __init__(self):
+    def __init__(
+            self,
+            speed       = 250.0,
+            pitch_angle = 0.0,
+            roll_angle  = 0.0,
+            heading_angle = 0.0,
+            altitude    = 6000.0
+            ):
+
+        """
+        aircraft navigational info
+        """
+
+        self.speed = speed
+        self.pitch_angle = pitch_angle
+        self.roll_angle = roll_angle
+        self.heading_angle = heading_angle
+
+    def camera(
+            self,
+            camera_type = 'all-sky',
+            field_of_view = 180.0,
+            sensor_zenith_angle = 0.0,
+            sensor_azimuth_angle = 0.0,
+            pitch_angle_offset = 0.0,
+            roll_angle_offset = 0.0
+            ):
+
+        """
+        camera info
+        """
+
+        self.camera = {
+            'camera_type'         : camera_type,
+            'field_of_view'       : field_of_view,
+            'sensor_zenith_angle' : sensor_zenith_angle,
+            'sensor_azimuth_angle': sensor_azimuth_angle,
+            'pitch_angle_offset'  : pitch_angle_offset,
+            'roll_angle_offset'   : roll_angle_offset
+            }
+
+    def region(
+            self,
+            Nx = 480,
+            Ny = 480,
+            dx = 100.0,
+            dy = 100.0,
+            extent = None,
+            Nx_pos = 0,
+            Ny_pos = 0
+            ):
+
+        """
+        basic region
+        """
 
         pass
+
+    def fly(
+            delta_seconds=1.0
+            ):
+
+        travel_dist_x = self.speed * delta_seconds * np.sin(np.deg2rad(self.heading_angle))
+        travel_dist_y = self.speed * delta_seconds * np.cos(np.deg2rad(self.heading_angle))
+
+        self.region['Nx_pos'] += travel_dist_x // self.region['dx']
+        self.region['Ny_pos'] += travel_dist_y // self.region['dy']
+
+
+    def flyover_view(
+            self,
+            photons = 1e8,
+            wavelength = 600.0,
+            overwrite = True
+            ):
+
+        # def run_rad_sim(aircraft, fname_nc, fdir0, wavelength=600, overwrite=True):
+
+        """
+        core function to run radiance simulation
+        """
+
+        fdir = '%s/%dnm' % (fdir0, wavelength)
+
+        if not os.path.exists(fdir):
+            os.makedirs(fdir)
+
+        levels = np.arange(0.0, 20.1, 1.0)
+
+        fname_atm = '%s/atm.pk' % fdir
+        atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
+        fname_abs = '%s/abs.pk' % fdir
+        abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
+
+        fname_les = '%s/les.pk' % fdir
+        cld0      = cld_les(fname_nc=fname_nc, fname=fname_les, altitude=atm0.lay['altitude']['data'], coarsing=[1, 1, 1, 1], overwrite=overwrite)
+
+        # radiance 3d
+        # =======================================================================================================
+        target    = 'radiance'
+        solver    = '3D'
+        atm1d0    = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+        atm3d0    = mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir)
+
+        # coarsen
+        atm3d0.nml['Atm_dx']['data'] *= coarsen_factor
+        atm3d0.nml['Atm_dy']['data'] *= coarsen_factor
+
+        atm_1ds   = [atm1d0]
+        atm_3ds   = [atm3d0]
+
+        mca0 = mcarats_ng(
+                date=datetime.datetime(2016, 8, 29),
+                atm_1ds=atm_1ds,
+                atm_3ds=atm_3ds,
+                Ng=abs0.Ng,
+                target=target,
+                surface_albedo=0.0,
+                solar_zenith_angle=29.162360459281544,
+                solar_azimuth_angle=-63.16777636586792,
+                sensor_zenith_angle=0.0,
+                sensor_azimuth_angle=0.0,
+                fdir='%s/%4.4d/rad_%s' % (fdir, wavelength, solver.lower()),
+                Nrun=3,
+                photons=photons,
+                weights=abs0.coef['weight']['data'],
+                solver=solver,
+                Ncpu=24,
+                mp_mode='py',
+                overwrite=overwrite)
+
+        out0 = mca_out_ng(fname='%s/mca-out-rad-%s_%.2fnm.h5' % (fdir0, solver.lower(), wavelength), mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
+        rad_3d      = out0.data['rad']['data']
+        rad_3d[np.isnan(rad_3d)] = 0.0
+        rad_3d[rad_3d<0.0] = 0.0
+
+        cot_true      = np.sum(cld0.lay['cot']['data'], axis=-1)
+        cot_true[np.isnan(cot_true)] = 0.0
+        cot_true[cot_true<0.0] = 0.0
+
+        cot_1d      = f_mca.interp_from_rad(rad_3d)
+        cot_1d[np.isnan(cot_1d)] = 0.0
+        cot_1d[cot_1d<0.0] = 0.0
+        # =======================================================================================================
+
+        fname_new = 'data/data_%s_coa-fac-%d_%dnm.h5' % (os.path.basename(fdir0), coarsen_factor, wavelength)
+        f = h5py.File(fname_new, 'w')
+
+        f['cot_true'] = cot_true
+        f['rad_3d']   = rad_3d
+        f['cot_1d']   = cot_1d
+
+        f.close()
+
+
+
 
 
 
@@ -146,7 +220,7 @@ if __name__ == '__main__':
     aircraft0 = aircraft()
     # =============================================================================
 
-    fname = 'data/les.nc'
+    # fname = 'data/les.nc'
 
     # step 2
     # run ERT for LES scenes at specified coarsening factor
@@ -155,5 +229,3 @@ if __name__ == '__main__':
     # for coarsen_factor in [1, 2, 4]:
     #     main_les(coarsen_factor=coarsen_factor)
     # =============================================================================
-
-    pass
