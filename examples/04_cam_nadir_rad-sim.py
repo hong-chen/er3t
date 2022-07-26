b9a4838b72c936ca11bd2cab364d7def732541ee
hong-chen <hong.chen.cu@gmail.com>
2022-07-26 15:32:00 -0600

    modified for publication (fully tested)

--- examples/04_cam_nadir_rad-sim.py
+++ examples/04_cam_nadir_rad-sim.py
@@ -1,30 +1,44 @@
-#!/bin/env python
+"""
+by Hong Chen (hong.chen.cu@gmail.com)
 
-#SBATCH --partition=shas
-#SBATCH --nodes=1
-#SBATCH --ntasks=24
-#SBATCH --ntasks-per-node=24
-#SBATCH --time=24:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=hong.chen-1@colorado.edu
-#SBATCH --output=sbatch-output_%x_%j.txt
-#SBATCH --job-name=les_04
+This code serves as an example code to reproduce 3D radiance simulations for App. 4 in Chen et al. (2022).
+
+The processes include:
+    1) `main_pre_ipa()`: pre-process all-sky camera data
+        a) read in camera radiance observations at red channel
+        b) convert radiance into reflectance and perform IPA method (two-stream approximation) to retrieve
+           cloud optical thickness (cot_ipa)
+
+    2) `main_pre_cnn()`: use CNN model to predict cloud optical thickness (cot_cnn) based on camera measured radiance
+        a) load CNN model
+        b) predict cot_cnn
+
+    3) `main_sim()`: use EaR3T to run 3D radiance simulations based on
+        a) cot_ipa
+        b) cot_cnn
+
+    4) `main_post()`: post-process data and plot
+        a) read in
+            i) camera observed radiance
+            j) 3D radiance simulation based on cot_ipa
+            k) 3D radiance simulation based on cot_cnn
+        b) plot
+
+This code has been tested under:
+    1) Linux on 2022-07-26 by Hong Chen
+      Operating System: Red Hat Enterprise Linux
+           CPE OS Name: cpe:/o:redhat:enterprise_linux:7.7:GA:workstation
+                Kernel: Linux 3.10.0-1062.9.1.el7.x86_64
+          Architecture: x86-64
+
+"""
 
 import os
 import h5py
-import glob
 import numpy as np
 import datetime
-import time
-from scipy.io import readsav
-from scipy.optimize import curve_fit
-from scipy.interpolate import interp1d
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FixedLocator
-from matplotlib import rcParams, ticker
-import matplotlib.gridspec as gridspec
-import matplotlib.patches as mpatches
 
 from er3t.pre.atm import atm_atmmod
 from er3t.pre.abs import abs_16g
@@ -36,231 +50,7 @@ from er3t.rtm.mca_v010 import mcarats_ng
 from er3t.rtm.mca_v010 import mca_out_ng
 
 
-def cal_solar_angles(dtime, longitude, latitude, altitude):
 
-    dtime = dtime.replace(tzinfo=datetime.timezone.utc)
-
-    sza_i = 90.0 - pysolar.solar.get_altitude(latitude, longitude, dtime, elevation=altitude)
-    if sza_i < 0.0 or sza_i > 90.0:
-        sza_i = np.nan
-
-    saa_i = pysolar.solar.get_azimuth(latitude, longitude, dtime, elevation=altitude)
-    if saa_i >= 0.0:
-        if 0.0<=saa_i<=180.0:
-            saa_i = 180.0 - saa_i
-        elif 180.0<saa_i<=360.0:
-            saa_i = 540.0 - saa_i
-        else:
-            saa_i = np.nan
-    elif saa_i < 0.0:
-        if -180.0<=saa_i<0.0:
-            saa_i = -saa_i + 180.0
-        elif -360.0<=saa_i<-180.0:
-            saa_i = -saa_i - 180.0
-        else:
-            saa_i = np.nan
-
-    return sza_i, saa_i
-
-class aircraft:
-
-    """
-    create an aircraft object
-    """
-
-    def __init__(
-            self,
-            date        = datetime.datetime(2019, 10, 5, 2),
-            speed       = 200.0,
-            pitch_angle = 0.0,
-            roll_angle  = 0.0,
-            heading_angle = 0.0,
-            altitude    = 6000.0
-            ):
-
-        """
-        aircraft navigational info
-        """
-
-        self.datetime = date
-        self.speed = speed
-        self.pitch_angle = pitch_angle
-        self.roll_angle = roll_angle
-        self.heading_angle = heading_angle
-        self.altitude = altitude
-
-    def install_camera(
-            self,
-            sensor_type = 'all-sky camera',
-            field_of_view = 180.0,
-            sensor_zenith_angle = 0.0,
-            sensor_azimuth_angle = 0.0,
-            pitch_angle_offset = 0.0,
-            roll_angle_offset = 0.0
-            ):
-
-        """
-        camera info
-        """
-
-        self.camera = {
-            'sensor_type'         : sensor_type,
-            'field_of_view'       : field_of_view,
-            'sensor_zenith_angle' : sensor_zenith_angle,
-            'sensor_azimuth_angle': sensor_azimuth_angle,
-            'pitch_angle_offset'  : pitch_angle_offset,
-            'roll_angle_offset'   : roll_angle_offset
-            }
-
-    def initialize_geoinfo(
-            self,
-            extent = [122.55, 123.0, 15.55, 16.0],
-            Nx = 480,
-            Ny = 480,
-            dx = 100.0,
-            dy = 100.0,
-            xpos = 0.2,
-            ypos = 0.1,
-            ):
-
-        """
-        geoinfo
-        """
-
-        lon = extent[0]+xpos*(extent[1]-extent[0])
-        lat = extent[2]+ypos*(extent[3]-extent[2])
-        sza, saa = cal_solar_angles(self.datetime, lon, lat, self.altitude)
-
-        self.geoinfo = {
-                'extent': extent,
-                'Nx': Nx,
-                'Ny': Ny,
-                'dx': dx,
-                'dy': dy,
-                'xpos': xpos,
-                'ypos': ypos,
-                'lon' : lon,
-                'lat' : lat,
-                'solar_zenith_angle': sza,
-                'solar_azimuth_angle': saa,
-                'Nfly': 0
-                }
-
-    def fly_to_next(
-            self,
-            delta_seconds=1.0
-            ):
-
-        travel_dist_x = self.speed * delta_seconds * np.sin(np.deg2rad(self.heading_angle))
-        travel_dist_y = self.speed * delta_seconds * np.cos(np.deg2rad(self.heading_angle))
-
-        self.geoinfo['xpos'] += (travel_dist_x // self.geoinfo['dx']) / self.geoinfo['Nx']
-        self.geoinfo['ypos'] += (travel_dist_y // self.geoinfo['dy']) / self.geoinfo['Ny']
-
-        lon   = self.geoinfo['extent'][0]+self.geoinfo['xpos']*(self.geoinfo['extent'][1]-self.geoinfo['extent'][0])
-        lat   = self.geoinfo['extent'][2]+self.geoinfo['ypos']*(self.geoinfo['extent'][3]-self.geoinfo['extent'][2])
-        self.geoinfo['lon']   = lon
-        self.geoinfo['lat']   = lat
-
-        self.datetime += datetime.timedelta(seconds=delta_seconds)
-
-        sza, saa = cal_solar_angles(self.datetime, lon, lat, self.altitude)
-        self.geoinfo['solar_zenith_angle'] = sza
-        self.geoinfo['solar_azimuth_angle'] = saa
-
-        self.geoinfo['Nfly'] += 1
-
-    def flyover_view(
-            self,
-            fdir0='tmp-data/06',
-            date = datetime.datetime(2019, 10, 5),
-            photons = 1e8,
-            solver = '3D',
-            wavelength = 600.0,
-            surface_albedo = 0.03,
-            plot=True,
-            overwrite=True
-            ):
-
-        # def run_rad_sim(aircraft, fname_nc, fdir0, wavelength=600, overwrite=True):
-
-        """
-        core function to run radiance simulation
-        """
-
-        fdir = '%s/%4.4dnm' % (fdir0, wavelength)
-        fdir_scene = '%s/scene_%3.3d' % (fdir, self.geoinfo['Nfly'])
-
-        if not os.path.exists(fdir_scene):
-            os.makedirs(fdir_scene)
-
-        # setup atmosphere (1D) and clouds (3D)
-        # =======================================================================================================
-        levels = np.arange(0.0, 20.1, 1.0)
-
-        fname_atm = '%s/atm.pk' % fdir0
-        atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=False)
-        fname_abs = '%s/abs.pk' % fdir0
-        abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=False)
-
-        fname_les = '%s/les.pk' % fdir0
-        # cld0      = cld_les(fname_nc='data/les.nc', fname=fname_les, coarsing=[1, 1, 25, 1], overwrite=False)
-        cld0      = cld_les(fname_nc='data/les.nc', altitude=levels, fname=fname_les, overwrite=False)
-
-        atm1d0    = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
-        atm3d0    = mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir0)
-
-        atm_1ds   = [atm1d0]
-        atm_3ds   = [atm3d0]
-        # =======================================================================================================
-
-        mca0 = mcarats_ng(
-                date=date,
-                atm_1ds=atm_1ds,
-                atm_3ds=atm_3ds,
-                Ng=abs0.Ng,
-                target='radiance',
-                surface_albedo=surface_albedo,
-                solar_zenith_angle=self.geoinfo['solar_zenith_angle'],
-                solar_azimuth_angle=self.geoinfo['solar_azimuth_angle'],
-                sensor_zenith_angle=self.camera['sensor_zenith_angle'],
-                sensor_azimuth_angle=self.camera['sensor_azimuth_angle'],
-                sensor_altitude = self.altitude,
-                sensor_type = self.camera['sensor_type'],
-                sensor_xpos = self.geoinfo['xpos'],
-                sensor_ypos = self.geoinfo['ypos'],
-                fdir='%s/rad_%s' % (fdir_scene, solver.lower()),
-                Nrun=3,
-                photons=photons,
-                weights=abs0.coef['weight']['data'],
-                solver=solver,
-                Ncpu=12,
-                mp_mode='py',
-                overwrite=overwrite)
-
-        fname_out = '%s/mca-out-rad-%s_%.2fnm_%3.3d.h5' % (fdir0, solver.lower(), wavelength, self.geoinfo['Nfly'])
-        out0 = mca_out_ng(fname=fname_out, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
-
-        # plot
-        # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        if plot:
-            fname_png = os.path.basename(fname_out).replace('.h5', '.png')
-
-            fig = plt.figure(figsize=(8, 6))
-            ax1 = fig.add_subplot(111)
-            cs = ax1.imshow(np.transpose(out0.data['rad']['data']), cmap='Greys_r', vmin=0.0, vmax=0.3, origin='lower')
-            plt.colorbar(cs)
-            ax1.set_xlabel('X Index')
-            ax1.set_ylabel('Y Index')
-            ax1.set_title('All-Sky Camera Radiance Simulation (%s Mode, %d nm, Scene %2.2d)' % (solver, wavelength, self.geoinfo['Nfly']))
-            plt.savefig(fname_png, bbox_inches='tight')
-            plt.close(fig)
-        # ------------------------------------------------------------------------------------------------------
-
-
-
-
-# passed test
 
 def get_cld_rtv_ipa(ref_2d, lon_2d, lat_2d, sza0):
 
@@ -544,9 +334,10 @@ def main_sim():
     # read in CNN based cloud data
     cloud_cnn = {}
     if os.path.exists('data/04_cam_nadir_rad-sim/pre-data_cnn.h5'):
-        f = h5py.File('data/04_cam_nadir_rad-sim/pre-data_cnn.h5', 'r')
+        fname = 'data/04_cam_nadir_rad-sim/pre-data_cnn.h5'
     else:
-        f = h5py.File('data/04_cam_nadir_rad-sim/aux/pre-data_cnn.h5', 'r')
+        fname = 'data/04_cam_nadir_rad-sim/aux/pre-data_cnn.h5'
+    f = h5py.File(fname, 'r')
     cot_2d = f['cnn/cot_wei'][...]
     cer_2d = np.zeros_like(cot_2d); cer_2d[...] = 12.0
     cloud_cnn['lon_2d'] = dict(name='Gridded longitude'               , units='degrees'    , data=f['lon'][...])
@@ -560,8 +351,107 @@ def main_sim():
     cal_mca_rad(date, geometry, cloud_ipa, wavelength=600.0, cth=2.0, photons=1e7, fdir='tmp-data/04_cam_nadir_rad-sim/ipa', solver='3D', overwrite=True)
     cal_mca_rad(date, geometry, cloud_cnn, wavelength=600.0, cth=2.0, photons=1e7, fdir='tmp-data/04_cam_nadir_rad-sim/cnn', solver='3D', overwrite=True)
 
-def main_post():
-    pass
+def main_post(plot=True):
+
+    # read in camera measured red channel radiance
+    f = h5py.File('data/04_cam_nadir_rad-sim/pre-data_ipa.h5', 'r')
+    extent = f['extent'][...]
+    rad_cam = f['rad'][...]
+    f.close()
+
+    # read in simulated 3D radiance based on cot_ipa
+    f = h5py.File('tmp-data/04_cam_nadir_rad-sim/ipa/mca-out-rad-cam-3d_600.0000nm.h5', 'r')
+    rad_sim_ipa = f['mean/rad'][...]
+    f.close()
+
+    # read in simulated 3D radiance based on cot_cnn
+    f = h5py.File('tmp-data/04_cam_nadir_rad-sim/cnn/mca-out-rad-cam-3d_600.0000nm.h5', 'r')
+    rad_sim_cnn = f['mean/rad'][...]
+    f.close()
+
+    # save data into <post-data.h5> under data/04_cam_nadir_rad-sim
+    f = h5py.File('data/04_cam_nadir_rad-sim/post-data.h5', 'w')
+    f['extent'] = extent
+    f['rad_cam'] = rad_cam
+    f['rad_sim-3d_cot-ipa'] = rad_sim_ipa
+    f['rad_sim-3d_cot-cnn'] = rad_sim_cnn
+    f.close()
+
+    if plot:
+
+        # =============================================================================
+        fig = plt.figure(figsize=(13, 8))
+        ax1 = fig.add_subplot(231)
+        ax1.imshow(rad_cam.T, extent=extent, origin='lower', cmap='Greys_r', vmin=0.0, vmax=0.6, alpha=0.3)
+        rad_cam0 = rad_cam.copy()
+        rad_cam0[-7:, :] = np.nan
+        rad_cam0[:, -7:] = np.nan
+        rad_cam0[:7, :] = np.nan
+        rad_cam0[:, :7] = np.nan
+        ax1.imshow(rad_cam0.T, extent=extent, origin='lower', cmap='jet', vmin=0.0, vmax=0.6, alpha=1.0)
+        ax1.set_xlabel('X [km]')
+        ax1.set_ylabel('Y [km]')
+        ax1.set_title('Radiance Observation (Red)')
+
+        ax2 = fig.add_subplot(232)
+        ax2.imshow(rad_sim_ipa.T, extent=extent, origin='lower', cmap='Greys_r', vmin=0.0, vmax=0.6, alpha=0.3)
+        rad_sim_ipa0 = rad_sim_ipa.copy()
+        rad_sim_ipa0[-7:, :] = np.nan
+        rad_sim_ipa0[:, -7:] = np.nan
+        rad_sim_ipa0[:7, :] = np.nan
+        rad_sim_ipa0[:, :7] = np.nan
+        ax2.imshow(rad_sim_ipa0.T, extent=extent, origin='lower', cmap='jet', vmin=0.0, vmax=0.6, alpha=1.0)
+        ax2.set_xlabel('X [km]')
+        ax2.set_ylabel('Y [km]')
+        ax2.set_title('3D Radiance Simulation (COT$\mathrm{_{IPA}}$)')
+
+        ax3 = fig.add_subplot(233)
+        xedges = np.arange(0.0, 0.61, 0.03)
+        yedges = np.arange(0.0, 0.61, 0.03)
+        heatmap, xedges, yedges = np.histogram2d(rad_cam0.ravel(), rad_sim_ipa0.ravel(), bins=(xedges, yedges))
+        extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+        YY, XX = np.meshgrid((yedges[:-1]+yedges[1:])/2.0, (xedges[:-1]+xedges[1:])/2.0)
+        levels = np.power(10, np.linspace(0.0, 1.5, 151))
+
+        cs = ax3.contourf(XX, YY, heatmap, levels, extend='both')
+        ax3.plot([0.0, 1.0], [0.0, 1.0], lw=1.5, color='gray', ls='--', zorder=2)
+        ax3.set_xlim([0.0, 0.6])
+        ax3.set_ylim([0.0, 0.6])
+        ax3.set_xlabel('Radiance Observation')
+        ax3.set_ylabel('Radiance Simulation (COT$\mathrm{_{IPA}}$)')
+
+        ax4 = fig.add_subplot(235)
+        ax4.imshow(rad_sim_cnn.T, extent=extent, origin='lower', cmap='Greys_r', vmin=0.0, vmax=0.6, alpha=0.3)
+        rad_sim_cnn0 = rad_sim_cnn.copy()
+        rad_sim_cnn0[-7:, :] = np.nan
+        rad_sim_cnn0[:, -7:] = np.nan
+        rad_sim_cnn0[:7, :] = np.nan
+        rad_sim_cnn0[:, :7] = np.nan
+        ax4.imshow(rad_sim_cnn0.T, extent=extent, origin='lower', cmap='jet', vmin=0.0, vmax=0.6, alpha=1.0)
+        ax4.set_xlabel('X [km]')
+        ax4.set_ylabel('Y [km]')
+        ax4.set_title('3D Radiance Simulation (COT$\mathrm{_{CNN}}$)')
+
+        ax5 = fig.add_subplot(236)
+        xedges = np.arange(0.0, 0.61, 0.03)
+        yedges = np.arange(0.0, 0.61, 0.03)
+        heatmap, xedges, yedges = np.histogram2d(rad_cam0.ravel(), rad_sim_cnn0.ravel(), bins=(xedges, yedges))
+        extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+        YY, XX = np.meshgrid((yedges[:-1]+yedges[1:])/2.0, (xedges[:-1]+xedges[1:])/2.0)
+        levels = np.power(10, np.linspace(0.0, 1.5, 151))
+
+        cs = ax5.contourf(XX, YY, heatmap, levels, extend='both')
+        ax5.plot([0.0, 1.0], [0.0, 1.0], lw=1.5, color='gray', ls='--', zorder=2)
+        ax5.set_xlim([0.0, 0.6])
+        ax5.set_ylim([0.0, 0.6])
+        ax5.set_xlabel('Radiance Observation')
+        ax5.set_ylabel('Radiance Simulation (COT$\mathrm{_{CNN}}$)')
+
+        plt.subplots_adjust(hspace=0.45, wspace=0.45)
+
+        plt.savefig('04_cam_nadir_rad-sim.png', bbox_inches='tight')
+        plt.show()
+        # =============================================================================
 
 
 
@@ -577,7 +467,8 @@ if __name__ == '__main__':
     # Step 2. Use CNN to predict cloud optical thickness from camera red channel radiance
     # Special note: to run the following function, tensorflow needs to be installed.
     #               If you have problems in setting up tensorflow enviroment, you can skip this step and
-    #               use <pre-data_cnn.h5> under data/04_cam_nadir_rad-sim/aux
+    #               use <pre-data_cnn.h5> provided under data/04_cam_nadir_rad-sim/aux instead.
+    #               CNN model credit: Nataraja et al. 2022 (https://doi.org/10.5194/amt-2022-45)
     # main_pre_cnn()
 
     # Step 3. Use EaR3T to run radiance simulations for both cot_ipa and cot_cnn
@@ -585,25 +476,12 @@ if __name__ == '__main__':
     #    b. 3D radiance simulation using cot_cnn
     # main_sim()
 
-    # Step 3. Post-process and plot
+    # Step 4. Post-process and plot
     #    a. save data in <post-data.h5> under data/04_cam_nadir_rad-sim
-    #        1) raw radiance measurements
+    #        1) raw camera radiance measurements
     #        2) radiance simulation based on cot_ipa
     #        3) radiance simulation based on cot_cnn
     #    b. plot
     # main_post()
 
-
-
-
-    # forward simulation
-    # =============================================================================
-    # aircraft0 = aircraft()
-    # aircraft0.install_camera()
-    # aircraft0.initialize_geoinfo()
-    # aircraft0.flyover_view()
-
-    # for i in range(20):
-    #     aircraft0.fly_to_next()
-    #     aircraft0.flyover_view()
-    # =============================================================================
+    pass
