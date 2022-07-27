f359ef679067a31d9f94365322ec6b70d995bccf
hong-chen <hong.chen.cu@gmail.com>
2022-07-27 10:58:24 -0600

    modified for publication

--- er3t/util/__init__.py
+++ er3t/util/__init__.py
@@ -2,7 +2,6 @@ from __future__ import division, print_function, absolute_import
 from .tools import *
 from .modis import *
 from .seviri import *
-from .cloud import *
 from .ahi import *
 
 __all__ = [s for s in dir() if not s.startswith('_')]
