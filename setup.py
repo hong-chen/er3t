325d0dcea7960da1be38a9339de067883bd975fc
hong-chen <hong.chen.cu@gmail.com>
2022-08-24 14:45:10 -0600

    changed version and status to Beta

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open(os.path.join(current_dir, 'README.rst'), encoding='utf-8') as f:
 
 setup(
      name = 'er3t',
-     version = '1.0.0',
+     version = '0.1.0',
      description = 'Education and Research 3D Radiative Transfer Toolbox',
      long_description = long_description,
      classifiers = [
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU GPLv3 License',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Atmospheric Science',
         ],
      keywords = '3D radiation radiative transfer',
@@ -24,8 +24,8 @@ setup(
      author_email = 'hong.chen.cu@gmail.com, sebastian.schmidt@lasp.colorado.edu',
      license = 'GNU GPLv3',
      packages = find_packages(),
-     install_requires = ['nose', 'numpy', 'scipy', 'h5py'],
-     python_requires = '~=3.7',
+     install_requires = ['nose', 'numpy', 'scipy', 'h5py', 'netCDF4', 'pyhdf', 'cartopy==0.19.0'],
+     python_requires = '~=3.9',
      scripts = ['bin/lss', 'bin/lsa'],
      include_package_data = True,
      zip_safe = False
