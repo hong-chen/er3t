cdc8aa4afddedbf3fb567246aac115c1e3409fbb
hong-chen <hong.chen.cu@gmail.com>
2022-08-25 14:58:59 -0600

    added file path for examples, tests, er3t etc.

--- er3t/common.py
+++ er3t/common.py
@@ -10,13 +10,18 @@ has_hdf4       = (importlib.util.find_spec('pyhdf') is not None)
 has_hdf5       = (importlib.util.find_spec('h5py') is not None)
 has_xarray     = (importlib.util.find_spec('xarray') is not None)
 
+fdir_er3t        = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+
 fdir_data        = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
-fdir_data_tmp    = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'tmp-data')
 fdir_data_abs    = os.path.join(fdir_data, 'abs')
 fdir_data_pha    = os.path.join(fdir_data, 'pha')
 fdir_data_atmmod = os.path.join(fdir_data, 'atmmod')
 fdir_data_slit   = os.path.join(fdir_data, 'slit')
 fdir_data_ssfr   = os.path.join(fdir_data_slit, 'ssfr')
+
+fdir_data_tmp    = os.path.join(fdir_er3t, 'tmp-data')
+fdir_examples    = os.path.join(fdir_er3t, 'examples')
+fdir_tests       = os.path.join(fdir_er3t, 'tests')
 
 references = [
         'Chen, H., Schmidt, S., Massie, S. T., Nataraja, V., Norgren, M. S., Gristey, J. J., Feingold,G., Holz, R. E., and Iwabuchi, H.: The Education and Research 3D Radiative Transfer Toolbox (EaR³T) - Towards the Mitigation of 3D Bias in Airborne and Spaceborne Passive Imagery Cloud Retrievals, Atmos. Meas. Tech. Discuss. [preprint], doi:10.5194/amt-2022-143, in review, 2022.'
