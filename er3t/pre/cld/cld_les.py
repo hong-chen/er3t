365cbe6b30a05852a53b222cd0babc9d37211bda
hong-chen <hong.chen.cu@gmail.com>
2022-08-26 17:59:58 -0600

    improved the code for LES data processing <cld_les>

--- er3t/pre/cld/cld_les.py
+++ er3t/pre/cld/cld_les.py
@@ -18,7 +18,7 @@ class cld_les:
     Input:
         fname_nc= : keyword argument, default=None, the file path of the original netCDF4 file
         fname=    : keyword argument, default=None, the file path of the Python pickle file
-        coarsen=  : keyword argument, default=[1, 1, 1, 1], the parameter to downscale the data in [x, y, z, t]
+        coarsen=  : keyword argument, default=[1, 1, 1], the parameter to downscale the data in [x, y, z]
         overwrite=: keyword argument, default=False, whether to overwrite or not
         verbose=  : keyword argument, default=False, verbose tag
 
@@ -45,9 +45,9 @@ class cld_les:
                  fname_nc  = None, \
                  fname     = None, \
                  altitude  = None, \
-                 coarsen   = [1, 1, 1, 1], \
+                 coarsen   = [1, 1, 1], \
                  overwrite = False, \
-                 verbose   = False):
+                 verbose   = True):
 
         self.verbose = verbose     # verbose tag
         self.fname   = fname       # file name of the pickle file
@@ -102,12 +102,8 @@ class cld_les:
         # pre process
         self.pre_les(fname_nc)
 
-        # downscale data if needed
-        if any([i!=1 for i in self.coarsen]):
-            self.downscale(self.coarsen)
-
         # post process
-        self.post_les()
+        self.post_les(self.altitude)
 
 
     def dump(self, fname):
@@ -191,7 +187,6 @@ class cld_les:
         cer_3d[np.logical_not(logic)] = 0.0
         #\-----------------------------------------------------------------------------\
 
-
         self.Nx = Nx
         self.Ny = Ny
         self.Nz = Nz
@@ -200,12 +195,12 @@ class cld_les:
         #/-----------------------------------------------------------------------------/
         self.lay = {}
 
-        self.lay['x']           = {'data':x/1000.0             , 'name':'X'          , 'units':'km'}
-        self.lay['y']           = {'data':y/1000.0             , 'name':'Y'          , 'units':'km'}
-        self.lay['nx']          = {'data':x.size               , 'name':'Nx'         , 'units':'N/A'}
-        self.lay['ny']          = {'data':y.size               , 'name':'Ny'         , 'units':'N/A'}
-        self.lay['dx']          = {'data':abs(x[1]-x[0])/1000.0, 'name':'dx'         , 'units':'km'}
-        self.lay['dy']          = {'data':abs(y[1]-y[0])/1000.0, 'name':'dy'         , 'units':'km'}
+        self.lay['x']           = {'data':x/1000.0             , 'name':'X' , 'units':'km'}
+        self.lay['y']           = {'data':y/1000.0             , 'name':'Y' , 'units':'km'}
+        self.lay['nx']          = {'data':x.size               , 'name':'Nx', 'units':'N/A'}
+        self.lay['ny']          = {'data':y.size               , 'name':'Ny', 'units':'N/A'}
+        self.lay['dx']          = {'data':abs(x[1]-x[0])/1000.0, 'name':'dx', 'units':'km'}
+        self.lay['dy']          = {'data':abs(y[1]-y[0])/1000.0, 'name':'dy', 'units':'km'}
 
         self.lay['altitude']    = {'data':z/1000.0, 'name':'Altitude'   , 'units':'km'}
         self.lay['pressure']    = {'data':p       , 'name':'Pressure'   , 'units':'mb'}
@@ -213,13 +208,14 @@ class cld_les:
         dz  = self.lay['altitude']['data'][1:]-self.lay['altitude']['data'][:-1]
         diff = np.abs(dz-dz[0])
         if any([i>1e-3 for i in diff]):
-            msg = 'Warning [cld_les]: Altitude is non-equidistant.'
+            msg = '\nWarning [cld_les]: Altitude is non-equidistant.'
             warnings.warn(msg)
+        dz = np.append(dz, dz[-1])
         self.lay['thickness']   = {'data':dz , 'name':'Layer thickness', 'units':'km'}
 
-        self.lay['temperature'] = {'data':t_3d  , 'name':'Temperature', 'units':'K'}
-        self.lay['extinction']  = {'data':ext_3d, 'name':'Extinction coefficients', 'units':'m^-1'}
-        self.lay['cer']         = {'data':cer_3d, 'name':'Cloud effective radius', 'units':'mm'}
+        self.lay['temperature'] = {'data':t_3d  , 'name':'Temperature (3D)'            , 'units':'K'}
+        self.lay['extinction']  = {'data':ext_3d, 'name':'Extinction coefficients (3D)', 'units':'m^-1'}
+        self.lay['cer']         = {'data':cer_3d, 'name':'Cloud effective radius (3D)' , 'units':'mm'}
         #\-----------------------------------------------------------------------------\
 
 
@@ -234,112 +230,70 @@ class cld_les:
             raise ValueError(msg)
 
         dz_ = z_[1:] - z_[:-1]
+        dz_ = np.append(dz_, dz_[-1])
         self.lev['altitude']  = {'data':z_ , 'name':'Altitude'       , 'units':'km'}
-        self.lay['thickness'] = {'data':dz_, 'name':'Layer thickness', 'units':'km'}
+        self.lev['thickness'] = {'data':dz_, 'name':'Layer thickness', 'units':'km'}
         #\-----------------------------------------------------------------------------\
 
 
-        if False:
-            z_km = z/1000.0
+    def post_les(self, altitude):
 
-            while altitude.min() > z_km.min():
-                altitude = np.append(altitude[0]-(altitude[1]-altitude[0]), altitude)
-            altitude[altitude<0.0] = 0.0
+        """
+        altitude: re-mapping cloud properties at given level-altitudes (under development, unavailable yet)
+        """
 
-            if altitude.max() < z_km.max():
-                msg = 'Error [cld_les]: The highest level of LES is higher than the highest level of given altitude.'
-                raise ValueError(msg)
-
-            alt_min0 = altitude[altitude <= z_km.min()].max()
-            alt_max0 = altitude[altitude >= z_km.max()].min()
-            altitude = altitude[(altitude>=alt_min0)&(altitude<=alt_max0)]
-
-            self.lay['altitude']    = {'data':altitude             , 'name':'Altitude'   , 'units':'km'}
-
-            self.Nx = Nx
-            self.Ny = Ny
-            self.Nz = self.lay['altitude']['data'].size
-            self.Nt = Nt
-
-            # for given altitude
-            # ==================================================================================
-            dz  = self.lay['altitude']['data'][1:]-self.lay['altitude']['data'][:-1]
-            dz0 = dz[0]
-            diff = np.abs(dz-dz0)
-            if any([i>0.001 for i in diff]):
-                msg = 'Warning [cld_les]: Non-equidistant intervals found in <dz>.'
-                warnings.warn(msg)
-            dz  = np.append(dz, dz0)
-            alt = np.append(self.lay['altitude']['data']-dz0/2.0, self.lay['altitude']['data'][-1]+dz0/2.0)
-            alt[alt<0.0] = 0.0
-            dz  = alt[1:] - alt[:-1]
-            self.lev['altitude']    = {'data':alt, 'name':'Altitude'       , 'units':'km'}
-            self.lay['thickness']   = {'data':dz , 'name':'Layer thickness', 'units':'km'}
-            # ==================================================================================
-
-            # for LES altitude
-            # ==================================================================================
-            dz_  = z_km[1:]-z_km[:-1]
-            dz0_ = dz_[0]
-            diff_ = np.abs(dz_-dz0_)
-            if any([i>0.001 for i in diff_]):
-                msg = 'Warning [cld_les]: Non-equidistant intervals found in <dz_>.'
-                warnings.warn(msg)
-            dz_  = np.append(dz_, dz0_)
-            alt_ = np.append(z_km-dz0_/2.0, z_km[-1]+dz0_/2.0)
-            alt_[alt_<0.0] = 0.0
-            dz_  = alt_[1:] - alt_[:-1]
-            # ==================================================================================
-
-            p_new      = np.zeros(self.Nz, dtype=p.dtype)
-            t_3d_new   = np.zeros((self.Nt, self.Nz, self.Ny, self.Nx), dtype=t_3d.dtype)
-            ext_3d_new = np.zeros((self.Nt, self.Nz, self.Ny, self.Nx), dtype=t_3d.dtype)
-            cer_3d_new = np.zeros((self.Nt, self.Nz, self.Ny, self.Nx), dtype=t_3d.dtype)
-
-            for i in range(self.Nz):
-                indices = np.where((z_km>=alt[i]) & (z_km<alt[i+1]))[0]
-                logic   = (z_km>=alt[i]) & (z_km<alt[i+1])
-                if logic.sum() > 0:
-                    p_new[i]               = p[logic].mean()
-                    t_3d_new[:, i, :, :]   = np.mean(t_3d[:, logic, :, :], axis=1)
-                    cer_3d_new[:, i, :, :] = np.mean(cer_3d[:, logic, :, :], axis=1)
-
-                    ext0 = np.zeros((self.Nt, self.Ny, self.Nx), dtype=ext_3d.dtype)
-                    for index in indices:
-                        ext0 += ext_3d[:, index, :, :] * dz_[index]
-                    ext_3d_new[:, i, :, :] = ext0 / dz[i]
-
-            self.lay['pressure']    = {'data':p_new       , 'name':'Pressure'   , 'units':'mb'}
-            self.lay['temperature'] = {'data':t_3d_new    , 'name':'Temperature', 'units':'K'}
-            self.lay['extinction']  = {'data':ext_3d_new  , 'name':'Extinction coefficients', 'units':'m^-1'}
-            self.lay['cer']         = {'data':cer_3d_new  , 'name':'Cloud effective radius' , 'units':'mm'}
-
-
-    def post_les(self):
-
+        # transpose netCDF data from (Nz, Ny, Nx) to (Nx, Ny, Nz)
+        #/----------------------------------------------------------------------------\#
         for key in self.lay.keys():
-
             if isinstance(self.lay[key]['data'], np.ndarray):
-                if self.lay[key]['data'].ndim == 4:
-                    self.lay[key]['data']  = np.transpose(self.lay[key]['data'])[:, :, :, 0]
+                if self.lay[key]['data'].ndim == 3:
+                    self.lay[key]['data'] = np.transpose(self.lay[key]['data'])
+        #\----------------------------------------------------------------------------/#
 
+        # downscale (usually in vertical z dimension)
+        #/----------------------------------------------------------------------------\#
+        if any([i!=1 for i in self.coarsen]):
+            self.downscale(self.coarsen)
+        #\----------------------------------------------------------------------------/#
+
+        # cloud optical thickness
+        #/----------------------------------------------------------------------------\#
         cot_3d = np.zeros_like(self.lay['extinction']['data'])
-        for i, dz in enumerate(self.lay['thickness']['data']):
-            cot_3d[:, :, i] = self.lay['extinction']['data'][:, :, i] * dz * 1000.0
+        for i, dz0 in enumerate(self.lay['thickness']['data']):
+            cot_3d[:, :, i] = self.lay['extinction']['data'][:, :, i] * dz0 * 1000.0
+        self.lay['cot'] = {'data':cot_3d, 'name':'Cloud optical thickness (3D)'}
 
-        self.lay['cot'] = {'data':cot_3d, 'name':'Cloud optical thickness'}
+        self.lev['cot_2d'] = {'data': np.sum(cot_3d, axis=-1), 'name': 'Cloud optical thickness (2D)'}
+        #\----------------------------------------------------------------------------/#
+
+        # placeholder for <altitude> implementation
+        #/----------------------------------------------------------------------------\#
+        #\----------------------------------------------------------------------------/#
 
 
     def downscale(self, coarsen):
 
-        dnx, dny, dnz, dnt = coarsen
+        # extract downscaling factors of dnx, dny, dnz from <coarsen>
+        #/----------------------------------------------------------------------------\#
+        if len(coarsen) == 3:
+            dnx, dny, dnz = coarsen
+        elif len(coarsen) == 4:
+            dnx, dny, dnz, dnt = coarsen
+            msg = '\nWarning [cld_les]: <coarsen=[dnx, dny, dnz, dnt]> will be deprecated in future release (will only support <coarsen=[dnx, dny, dnz]>)'
+            warnings.warn(msg)
+        else:
+            msg = 'Error [cld_les]: Cannot interpret <coarsen> factors.'
+            raise OSError(msg)
+        #\----------------------------------------------------------------------------/#
 
-        if (self.Nx%dnx != 0) or (self.Ny%dny != 0) or \
-           (self.Nz%dnz != 0) or (self.Nt%dnt != 0):
+        # downscale in process
+        #/----------------------------------------------------------------------------\#
+        if (self.Nx%dnx != 0) or (self.Ny%dny != 0) or (self.Nz%dnz != 0):
             msg = 'Error [cld_les]: The original dimension %s is not divisible with %s, please check input (dnx, dny, dnz, dnt).' % (str(self.lay['temperature']['data'].shape), str(coarsen))
             raise ValueError(msg)
+
         else:
-            new_shape = (self.Nt//dnt, self.Nz//dnz, self.Ny//dny, self.Nx//dnx)
+            new_shape = (self.Nx//dnx, self.Ny//dny, self.Nz//dnz)
 
             if self.verbose:
                 print('Message [cld_les]: Downscaling data from dimension %s to %s ...' % (str(self.lay['temperature']['data'].shape), str(new_shape)))
@@ -352,17 +306,20 @@ class cld_les:
 
             self.lay['dx']['data'] *= dnx
             self.lay['dy']['data'] *= dny
+            self.lay['nx']['data'] = self.Nx//dnx
+            self.lay['ny']['data'] = self.Ny//dny
 
             for key in self.lay.keys():
                 if isinstance(self.lay[key]['data'], np.ndarray):
-                    if self.lay[key]['data'].ndim == len(coarsen):
+                    if self.lay[key]['data'].ndim >= 3:
                         self.lay[key]['data']  = downscale(self.lay[key]['data'], new_shape, operation='mean')
+
+            self.Nx = self.lay['nx']['data']
+            self.Ny = self.lay['ny']['data']
+        #\----------------------------------------------------------------------------/#
 
 
 
 if __name__ == '__main__':
 
-    import er3t.common
-    fname_nc = os.path.abspath('%s/data/00_er3t_mca/aux/les.nc' % er3t.common.fdir_examples)
-    cld0 = cld_les(fname_nc=fname_nc, coarsen=[1, 1, 10, 1])
     pass
