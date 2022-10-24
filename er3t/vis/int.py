495115e75e37a943df196773034649b05d618f2c
hong-chen <hong.chen.cu@gmail.com>
2022-10-24 14:40:47 -0600

    initiated implementation of interactive data visualization using bokeh

---
+++ er3t/vis/int.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import datetime
+from io import StringIO
+import numpy as np
+import h5py
+from scipy import interpolate
+import shutil
+import urllib.request
+import requests
+from er3t.util import check_equal, get_doy_tag, get_data_h4
+
+
+
+__all__ = ['compare_data_2d']
+
+
+def compare_data_2d(data_x, data_y):
+
+    pass
+
+if __name__=='__main__':
+
+    pass
