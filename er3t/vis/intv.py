cf2e882ca0f32f2d78c675f0fedf2e0a49676b29
hong-chen <hong.chen.cu@gmail.com>
2022-10-24 16:03:41 -0600

    changed name

--- er3t/vis/intv.py
+++ er3t/vis/intv.py
@@ -15,9 +15,12 @@ from er3t.util import check_equal, get_doy_tag, get_data_h4
 __all__ = ['compare_data_2d']
 
 
+
 def compare_data_2d(data_x, data_y):
 
     pass
+
+
 
 if __name__=='__main__':
 
