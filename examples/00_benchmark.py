d3fd62b25743a5f5a46e96f713a2c2cc2c360960
hong-chen <hong.chen.cu@gmail.com>
2023-01-31 10:26:25 -0700

    placeholder for benchmark

--- examples/00_benchmark.py
+++ examples/00_benchmark.py
@@ -1,3 +1,31 @@
 """
 Under development ...
 """
+
+
+class benchmark_ipa:
+
+    def __init__(self):
+
+        pass
+
+
+
+class benchmark_3d:
+
+    """
+    under development:
+
+    intend to benchmark 3d calculations using I3RC cases
+    """
+
+    def __init__(self):
+
+        pass
+
+
+if __name__ == '__main__':
+
+    bm_ipa = benchmark_ipa()
+
+    bm_3d  = benchmark_3d()
