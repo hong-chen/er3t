16929f36d1aa8103caac5ecdee14597859d529aa
hong-chen <hong.chen.cu@gmail.com>
2023-02-16 16:50:11 -0700

    testing rrtmg abs implementation

--- tests/02_test_abs.py
+++ tests/02_test_abs.py
@@ -1,7 +1,21 @@
 import os
+import sys
 import time
 import numpy as np
 import datetime
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.path as mpl_path
+import matplotlib.image as mpl_img
+import matplotlib.patches as mpatches
+import matplotlib.gridspec as gridspec
+from matplotlib import rcParams, ticker
+from matplotlib.ticker import FixedLocator
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+# import cartopy.crs as ccrs
+# mpl.use('Agg')
+
 
 from er3t.pre.atm import atm_atmmod
 from er3t.pre.abs import abs_16g, abs_oco_idl
@@ -154,7 +168,71 @@ def main():
 
 
 
+def test_abs_rrtmg(fdir='tmp-data'):
+
+    # create atm file
+    #/----------------------------------------------------------------------------\#
+    levels = np.linspace(0.0, 20.0, 41)
+    fname_atm  = '%s/atm.pk' % fdir
+    atm0 = atm_atmmod(levels=levels, fname=fname_atm, overwrite=True)
+    #\----------------------------------------------------------------------------/#
+
+    # read out abs coef for USSA (US standard atmosphere), provided by Xiuhong Chen
+    #/----------------------------------------------------------------------------\#
+    fname    = 'data/coef_sw_ussa.txt'
+    data     = np.genfromtxt(fname)
+    altitude = data[:, 0]
+    Nz = altitude.size
+    Nb = 14
+    Ng = 16
+    coef_sw  = data[:, 1:].reshape((Nz, Nb, Ng))
+    #\----------------------------------------------------------------------------/#
+
+    # figures
+    #/----------------------------------------------------------------------------\#
+    if True:
+        for i in range(Nb):
+
+            colors = mpl.cm.jet(np.linspace(0.0, 1.0, Ng))
+
+            # figure
+            #/----------------------------------------------------------------------------\#
+            plt.close('all')
+            fig = plt.figure(figsize=(6, 12))
+            # plot
+            #/--------------------------------------------------------------\#
+            ax1 = fig.add_subplot(111)
+
+            for j in range(Ng):
+                ax1.plot(coef_sw[:, i, j], altitude, color=colors[j, ...], lw=1.5, label='g %d' % (j+1))
+
+            # ax1.set_xlim((0, 0.08))
+            ax1.set_ylim((0, 70))
+            ax1.set_xlabel('Absorption Coefficient [m$^{-1}$]')
+            ax1.set_ylabel('Altitude [km]')
+            ax1.set_title('Band %d' % (i+1))
+
+            plt.legend(fontsize=16)
+            #\--------------------------------------------------------------/#
+            # save figure
+            #/--------------------------------------------------------------\#
+            plt.subplots_adjust(hspace=0.3, wspace=0.3)
+            _metadata = {'Computer': os.uname()[1], 'Script': os.path.abspath(__file__), 'Function':sys._getframe().f_code.co_name, 'Date':datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
+            plt.savefig('%s_band-%2.2d.png' % (_metadata['Function'], i+1), bbox_inches='tight', metadata=_metadata)
+            #\--------------------------------------------------------------/#
+            #\----------------------------------------------------------------------------/#
+    #\----------------------------------------------------------------------------/#
+
+    # create abs file, but we will need to input the created atm file
+    #/----------------------------------------------------------------------------\#
+    # fname_abs  = '%s/abs.pk' % fdir
+    # wavelength = 500.0
+    # abs_obj = abs_16g(wavelength=wavelength, atm_obj=atm0, verbose=True)
+    #\----------------------------------------------------------------------------/#
+    sys.exit()
+
+
 if __name__ == '__main__':
 
 
-    main()
+    test_abs_rrtmg()
