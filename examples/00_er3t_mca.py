3433f072c02a0984fa8bf1c4238ca5b4cdd981b1
hong-chen <hong.chen.cu@gmail.com>
2023-02-18 16:11:35 -0700

    changed the import procedure

--- examples/00_er3t_mca.py
+++ examples/00_er3t_mca.py
@@ -2,7 +2,7 @@
 by Hong Chen (hong.chen.cu@gmail.com)
 
 This code has been tested under:
-    1) Linux on 2022-10-20 by Hong Chen
+    1) Linux on 2023-02-18 by Hong Chen
       Operating System: Red Hat Enterprise Linux
            CPE OS Name: cpe:/o:redhat:enterprise_linux:7.7:GA:workstation
                 Kernel: Linux 3.10.0-1062.9.1.el7.x86_64
@@ -24,20 +24,8 @@ from matplotlib import rcParams
 import matplotlib.gridspec as gridspec
 import matplotlib.patches as mpatches
 
-from er3t.pre.atm import atm_atmmod
-from er3t.pre.abs import abs_16g
-from er3t.pre.cld import cld_les, cld_sat
-from er3t.pre.cld import cld_gen_hem as cld_gen
-from er3t.pre.sfc import sfc_sat
-from er3t.pre.pha import pha_mie_wc as pha_mie
+import er3t
 
-from er3t.rtm.mca import mca_atm_1d, mca_atm_3d, mca_sfc_2d
-from er3t.rtm.mca import mcarats_ng
-from er3t.rtm.mca import mca_out_ng
-from er3t.rtm.mca import mca_sca
-from er3t.util import cal_r_twostream
-
-import er3t.common
 
 
 
@@ -77,7 +65,7 @@ def test_01_flux_clear_sky(
     fname_atm = '%s/atm.pk' % fdir
 
     # atmosphere object
-    atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
+    atm0      = er3t.pre.atm.atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
 
     # data can be accessed at
     #     atm0.lev['altitude']['data']
@@ -106,7 +94,7 @@ def test_01_flux_clear_sky(
     fname_abs = '%s/abs.pk' % fdir
 
     # absorption object
-    abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
+    abs0      = er3t.pre.abs.abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
 
     # data can be accessed at
     #     abs0.coef['wavelength']['data']
@@ -120,7 +108,7 @@ def test_01_flux_clear_sky(
     # define mcarats 1d and 3d "atmosphere", can represent aersol, cloud, atmosphere
     #/-----------------------------------------------------------------------------\
     # homogeneous 1d mcarats "atmosphere"
-    atm1d0  = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+    atm1d0  = er3t.rtm.mca.mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
     # data can be accessed at
     #     atm1d0.nml[ig]['Atm_zgrd0']['data']
     #     atm1d0.nml[ig]['Atm_wkd0']['data']
@@ -142,7 +130,7 @@ def test_01_flux_clear_sky(
     # define mcarats object
     #/-----------------------------------------------------------------------------\
     # run mcarats
-    mca0 = mcarats_ng(
+    mca0 = er3t.rtm.mca.mcarats_ng(
             atm_1ds=atm_1ds,
             atm_3ds=atm_3ds,
             Ng=abs0.Ng,
@@ -179,7 +167,7 @@ def test_01_flux_clear_sky(
     # out0 = mca_out_ng(fname='mca-out-flux-3d_les.h5', mca_obj=mca0, abs_obj=abs0, mode='all' , squeeze=True, verbose=True, overwrite=True)
 
     fname_h5 = '%s/mca-out-flux-%s_%s.h5' % (fdir, solver.lower(), _metadata['Function'])
-    out0 = mca_out_ng(fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
+    out0 = er3t.rtm.mca.mca_out_ng(fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
 
     # data can be accessed at
     #     out0.data['f_up']['data']
@@ -258,7 +246,7 @@ def test_02_flux_les_cloud_3d(
     fname_atm = '%s/atm.pk' % fdir
 
     # atmosphere object
-    atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
+    atm0      = er3t.pre.atm.atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
 
     # data can be accessed at
     #     atm0.lev['altitude']['data']
@@ -287,7 +275,7 @@ def test_02_flux_les_cloud_3d(
     fname_abs = '%s/abs.pk' % fdir
 
     # absorption object
-    abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
+    abs0      = er3t.pre.abs.abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
 
     # data can be accessed at
     #     abs0.coef['wavelength']['data']
@@ -307,7 +295,7 @@ def test_02_flux_les_cloud_3d(
     fname_les = '%s/les.pk' % fdir
 
     # cloud object
-    cld0      = cld_les(fname_nc=fname_nc, fname=fname_les, coarsen=[1, 1, 25], overwrite=overwrite)
+    cld0      = er3t.pre.cld.cld_les(fname_nc=fname_nc, fname=fname_les, coarsen=[1, 1, 25], overwrite=overwrite)
 
     # data can be accessed at
     #     cld0.lay['x']['data']
@@ -327,7 +315,7 @@ def test_02_flux_les_cloud_3d(
     # define mcarats 1d and 3d "atmosphere", can represent aersol, cloud, atmosphere
     #/-----------------------------------------------------------------------------\
     # inhomogeneous 3d mcarats "atmosphere"
-    atm3d0  = mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir)
+    atm3d0  = er3t.rtm.mca.mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir)
     # data can be accessed at
     #     atm3d0.nml['Atm_nx']['data']
     #     atm3d0.nml['Atm_ny']['data']
@@ -342,7 +330,7 @@ def test_02_flux_les_cloud_3d(
     #     atm3d0.nml['Atm_apfp3d']['data']
 
     # homogeneous 1d mcarats "atmosphere"
-    atm1d0  = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+    atm1d0  = er3t.rtm.mca.mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
     # data can be accessed at
     #     atm1d0.nml[ig]['Atm_zgrd0']['data']
     #     atm1d0.nml[ig]['Atm_wkd0']['data']
@@ -364,7 +352,7 @@ def test_02_flux_les_cloud_3d(
     # define mcarats object
     #/-----------------------------------------------------------------------------\
     # run mcarats
-    mca0 = mcarats_ng(
+    mca0 = er3t.rtm.mca.mcarats_ng(
             date=datetime.datetime(2017, 8, 13),
             atm_1ds=atm_1ds,
             atm_3ds=atm_3ds,
@@ -402,7 +390,7 @@ def test_02_flux_les_cloud_3d(
     # out0 = mca_out_ng(fname='mca-out-flux-3d_les.h5', mca_obj=mca0, abs_obj=abs0, mode='all' , squeeze=True, verbose=True, overwrite=True)
 
     fname_h5 = '%s/mca-out-flux-%s_%s.h5' % (fdir, solver.lower(), _metadata['Function'])
-    out0 = mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
+    out0 = er3t.rtm.mca.mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
 
     # data can be accessed at
     #     out0.data['f_up']['data']
@@ -476,7 +464,7 @@ def test_03_flux_les_cloud_3d_aerosol_1d(
     fname_atm = '%s/atm.pk' % fdir
 
     # atmosphere object
-    atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
+    atm0      = er3t.pre.atm.atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
 
     # data can be accessed at
     #     atm0.lev['altitude']['data']
@@ -505,7 +493,7 @@ def test_03_flux_les_cloud_3d_aerosol_1d(
     fname_abs = '%s/abs.pk' % fdir
 
     # absorption object
-    abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
+    abs0      = er3t.pre.abs.abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
 
     # data can be accessed at
     #     abs0.coef['wavelength']['data']
@@ -525,7 +513,7 @@ def test_03_flux_les_cloud_3d_aerosol_1d(
     fname_les = '%s/les.pk' % fdir
 
     # cloud object
-    cld0      = cld_les(fname_nc=fname_nc, fname=fname_les, coarsen=[1, 1, 25], overwrite=overwrite)
+    cld0      = er3t.pre.cld.cld_les(fname_nc=fname_nc, fname=fname_les, coarsen=[1, 1, 25], overwrite=overwrite)
 
     # data can be accessed at
     #     cld0.lay['x']['data']
@@ -545,7 +533,7 @@ def test_03_flux_les_cloud_3d_aerosol_1d(
     # define mcarats 1d and 3d "atmosphere", can represent aersol, cloud, atmosphere
     #/-----------------------------------------------------------------------------\
     # inhomogeneous 3d mcarats "atmosphere"
-    atm3d0  = mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir)
+    atm3d0  = er3t.rtm.mca.mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir)
     # data can be accessed at
     #     atm3d0.nml['Atm_nx']['data']
     #     atm3d0.nml['Atm_ny']['data']
@@ -560,7 +548,7 @@ def test_03_flux_les_cloud_3d_aerosol_1d(
     #     atm3d0.nml['Atm_apfp3d']['data']
 
     # homogeneous 1d mcarats "atmosphere"
-    atm1d0  = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+    atm1d0  = er3t.rtm.mca.mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
 
     # add homogeneous 1d mcarats "atmosphere", aerosol layer
     aod    = 0.4 # aerosol optical depth
@@ -592,7 +580,7 @@ def test_03_flux_les_cloud_3d_aerosol_1d(
     # define mcarats object
     #/-----------------------------------------------------------------------------\
     # run mcarats
-    mca0 = mcarats_ng(
+    mca0 = er3t.rtm.mca.mcarats_ng(
             date=datetime.datetime(2017, 8, 13),
             atm_1ds=atm_1ds,
             atm_3ds=atm_3ds,
@@ -629,7 +617,7 @@ def test_03_flux_les_cloud_3d_aerosol_1d(
     # out0 = mca_out_ng(fname='mca-out-flux-3d_les.h5', mca_obj=mca0, abs_obj=abs0, mode='all' , squeeze=True, verbose=True, overwrite=True)
 
     fname_h5 = '%s/mca-out-flux-%s_%s.h5' % (fdir, solver.lower(), _metadata['Function'])
-    out0 = mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
+    out0 = er3t.rtm.mca.mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
 
     # data can be accessed at
     #     out0.data['f_up']['data']
@@ -703,7 +691,7 @@ def test_04_flux_les_cloud_3d_aerosol_3d(
     fname_atm = '%s/atm.pk' % fdir
 
     # atmosphere object
-    atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
+    atm0      = er3t.pre.atm.atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
 
     # data can be accessed at
     #     atm0.lev['altitude']['data']
@@ -732,7 +720,7 @@ def test_04_flux_les_cloud_3d_aerosol_3d(
     fname_abs = '%s/abs.pk' % fdir
 
     # absorption object
-    abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
+    abs0      = er3t.pre.abs.abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
 
     # data can be accessed at
     #     abs0.coef['wavelength']['data']
@@ -752,7 +740,7 @@ def test_04_flux_les_cloud_3d_aerosol_3d(
     fname_les = '%s/les.pk' % fdir
 
     # cloud object
-    cld0      = cld_les(fname_nc=fname_nc, fname=fname_les, coarsen=[1, 1, 25], overwrite=overwrite)
+    cld0      = er3t.pre.cld.cld_les(fname_nc=fname_nc, fname=fname_les, coarsen=[1, 1, 25], overwrite=overwrite)
 
     # data can be accessed at
     #     cld0.lay['x']['data']
@@ -772,7 +760,7 @@ def test_04_flux_les_cloud_3d_aerosol_3d(
     # define mcarats 1d and 3d "atmosphere", can represent aersol, cloud, atmosphere
     #/-----------------------------------------------------------------------------\
     # inhomogeneous 3d mcarats "atmosphere"
-    atm3d0  = mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir, overwrite=False)
+    atm3d0  = er3t.rtm.mca.mca_atm_3d(cld_obj=cld0, atm_obj=atm0, fname='%s/mca_atm_3d.bin' % fdir, overwrite=False)
 
     # 3d aerosol near surface (mixed with clouds)
     #/-----------------------------------------------------------------------------\
@@ -803,7 +791,7 @@ def test_04_flux_les_cloud_3d_aerosol_3d(
     #     atm3d0.nml['Atm_apfp3d']['data']
 
     # homogeneous 1d mcarats "atmosphere"
-    atm1d0  = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+    atm1d0  = er3t.rtm.mca.mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
     # data can be accessed at
     #     atm1d0.nml[ig]['Atm_zgrd0']['data']
     #     atm1d0.nml[ig]['Atm_wkd0']['data']
@@ -825,7 +813,7 @@ def test_04_flux_les_cloud_3d_aerosol_3d(
     # define mcarats object
     #/-----------------------------------------------------------------------------\
     # run mcarats
-    mca0 = mcarats_ng(
+    mca0 = er3t.rtm.mca.mcarats_ng(
             date=datetime.datetime(2017, 8, 13),
             atm_1ds=atm_1ds,
             atm_3ds=atm_3ds,
@@ -863,7 +851,7 @@ def test_04_flux_les_cloud_3d_aerosol_3d(
     # out0 = mca_out_ng(fname='mca-out-flux-3d_les.h5', mca_obj=mca0, abs_obj=abs0, mode='all' , squeeze=True, verbose=True, overwrite=True)
 
     fname_h5 = '%s/mca-out-flux-%s_%s.h5' % (fdir, solver.lower(), _metadata['Function'])
-    out0 = mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
+    out0 = er3t.rtm.mca.mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
 
     # data can be accessed at
     #     out0.data['f_up']['data']
@@ -940,7 +928,7 @@ def test_05_rad_les_cloud_3d(
     fname_atm = '%s/atm.pk' % fdir
 
     # atmosphere object
-    atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
+    atm0      = er3t.pre.atm.atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
 
     # data can be accessed at
     #     atm0.lev['altitude']['data']
@@ -969,7 +957,7 @@ def test_05_rad_les_cloud_3d(
     fname_abs = '%s/abs.pk' % fdir
 
     # absorption object
-    abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
+    abs0      = er3t.pre.abs.abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
 
     # data can be accessed at
     #     abs0.coef['wavelength']['data']
@@ -989,7 +977,7 @@ def test_05_rad_les_cloud_3d(
     fname_les = '%s/les.pk' % fdir
 
     # cloud object
-    cld0      = cld_les(fname_nc=fname_nc, fname=fname_les, coarsen=[1, 1, 25], overwrite=overwrite)
+    cld0      = er3t.pre.cld.cld_les(fname_nc=fname_nc, fname=fname_les, coarsen=[1, 1, 25], overwrite=overwrite)
 
     # data can be accessed at
     #     cld0.lay['x']['data']
@@ -1008,15 +996,15 @@ def test_05_rad_les_cloud_3d(
 
     # define mca_sca object
     #/-----------------------------------------------------------------------------\
-    pha0 = pha_mie(wavelength=wavelength)
-    sca  = mca_sca(pha_obj=pha0, fname='%s/mca_sca.bin' % fdir, overwrite=overwrite)
+    pha0 = er3t.pre.pha.pha_mie_wc(wavelength=wavelength)
+    sca  = er3t.rtm.mca.mca_sca(pha_obj=pha0, fname='%s/mca_sca.bin' % fdir, overwrite=overwrite)
     #\-----------------------------------------------------------------------------/
 
 
     # define mcarats 1d and 3d "atmosphere", can represent aersol, cloud, atmosphere
     #/-----------------------------------------------------------------------------\
     # inhomogeneous 3d mcarats "atmosphere"
-    atm3d0  = mca_atm_3d(cld_obj=cld0, atm_obj=atm0, pha_obj=pha0, fname='%s/mca_atm_3d.bin' % fdir, overwrite=overwrite)
+    atm3d0  = er3t.rtm.mca.mca_atm_3d(cld_obj=cld0, atm_obj=atm0, pha_obj=pha0, fname='%s/mca_atm_3d.bin' % fdir, overwrite=overwrite)
     # data can be accessed at
     #     atm3d0.nml['Atm_nx']['data']
     #     atm3d0.nml['Atm_ny']['data']
@@ -1031,7 +1019,7 @@ def test_05_rad_les_cloud_3d(
     #     atm3d0.nml['Atm_apfp3d']['data']
 
     # homogeneous 1d mcarats "atmosphere"
-    atm1d0  = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+    atm1d0  = er3t.rtm.mca.mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
     # data can be accessed at
     #     atm1d0.nml[ig]['Atm_zgrd0']['data']
     #     atm1d0.nml[ig]['Atm_wkd0']['data']
@@ -1054,7 +1042,7 @@ def test_05_rad_les_cloud_3d(
     # define mcarats object
     #/-----------------------------------------------------------------------------\
     # run mcarats
-    mca0 = mcarats_ng(
+    mca0 = er3t.rtm.mca.mcarats_ng(
             date=datetime.datetime(2017, 8, 13),
             atm_1ds=atm_1ds,
             atm_3ds=atm_3ds,
@@ -1097,7 +1085,7 @@ def test_05_rad_les_cloud_3d(
     # out0 = mca_out_ng(fname='mca-out-rad-3d_les.h5', mca_obj=mca0, abs_obj=abs0, mode='all' , squeeze=True, verbose=True, overwrite=True)
 
     fname_h5 = '%s/mca-out-rad-%s_%s.h5' % (fdir, solver.lower(), _metadata['Function'])
-    out0 = mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
+    out0 = er3t.rtm.mca.mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
 
     # data can be accessed at
     #     out0.data['rad']['data']
@@ -1155,14 +1143,14 @@ def test_06_rad_cld_gen_hem(
     #/-----------------------------------------------------------------------------\
     levels    = np.linspace(0.0, 20.0, 201)
     fname_atm = '%s/atm.pk' % fdir
-    atm0      = atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
+    atm0      = er3t.pre.atm.atm_atmmod(levels=levels, fname=fname_atm, overwrite=overwrite)
     #\-----------------------------------------------------------------------------/
 
 
     # define an absorption object
     #/-----------------------------------------------------------------------------\
     fname_abs = '%s/abs.pk' % fdir
-    abs0      = abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
+    abs0      = er3t.pre.abs.abs_16g(wavelength=wavelength, fname=fname_abs, atm_obj=atm0, overwrite=overwrite)
     #\-----------------------------------------------------------------------------/
 
 
@@ -1178,7 +1166,7 @@ def test_06_rad_cld_gen_hem(
     # <overwrite>     : whether to overwrite
     #/-----------------------------------------------------------------------------\
     fname_cld = '%s/cld.pk' % fdir
-    cld0 = cld_gen(
+    cld0 = er3t.pre.cld.cld_gen_hem(
             fname=fname_cld,
             radii=[1.0, 2.0, 4.0],
             weights=[0.6, 0.3, 0.1],
@@ -1228,15 +1216,15 @@ def test_06_rad_cld_gen_hem(
 
     # define mca_sca object
     #/-----------------------------------------------------------------------------\
-    pha0 = pha_mie(wavelength=wavelength)
-    sca  = mca_sca(pha_obj=pha0, fname='%s/mca_sca.bin' % fdir, overwrite=overwrite)
+    pha0 = er3t.pre.pha.pha_mie_wc(wavelength=wavelength)
+    sca  = er3t.rtm.mca.mca_sca(pha_obj=pha0, fname='%s/mca_sca.bin' % fdir, overwrite=overwrite)
     #\-----------------------------------------------------------------------------/
 
 
     # define mcarats 1d and 3d "atmosphere", can represent aersol, cloud, atmosphere
     #/-----------------------------------------------------------------------------\
-    atm1d0  = mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
-    atm3d0  = mca_atm_3d(fname='%s/mca_atm_3d.bin' % fdir, cld_obj=cld0, atm_obj=atm0, pha_obj=pha0, overwrite=overwrite)
+    atm1d0  = er3t.rtm.mca.mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+    atm3d0  = er3t.rtm.mca.mca_atm_3d(fname='%s/mca_atm_3d.bin' % fdir, cld_obj=cld0, atm_obj=atm0, pha_obj=pha0, overwrite=overwrite)
 
     atm_1ds   = [atm1d0]
     atm_3ds   = [atm3d0]
@@ -1246,7 +1234,7 @@ def test_06_rad_cld_gen_hem(
     # define mcarats object
     #/-----------------------------------------------------------------------------\
     # run mcarats
-    mca0 = mcarats_ng(
+    mca0 = er3t.rtm.mca.mcarats_ng(
             date=datetime.datetime(2017, 8, 13),
             atm_1ds=atm_1ds,
             atm_3ds=atm_3ds,
@@ -1274,7 +1262,7 @@ def test_06_rad_cld_gen_hem(
     # define mcarats output object
     #/-----------------------------------------------------------------------------\
     fname_h5 = '%s/mca-out-rad-%s_%s.h5' % (fdir, solver.lower(), _metadata['Function'])
-    out0 = mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
+    out0 = er3t.rtm.mca.mca_out_ng(fname=fname_h5, mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=overwrite)
     #\-----------------------------------------------------------------------------/
 
 
