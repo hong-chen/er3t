1c836a3168294252c584fec0e24d4071a485fb44
hong-chen <hong.chen.cu@gmail.com>
2023-06-19 21:19:45 -0600

    implementing <grid_by_dxdy> for equidistant gridding

--- er3t/util/util.py
+++ er3t/util/util.py
@@ -10,7 +10,7 @@ import numpy as np
 from scipy import interpolate, stats
 import warnings
 
-import er3t.common
+import er3t
 
 
 
@@ -461,6 +461,96 @@ def grid_by_lonlat(lon, lat, data, lon_1d=None, lat_1d=None, method='nearest'):
         return lon_2d, lat_2d, data_2d
     else:
         data_2d0 = interpolate.griddata(points, data, (lon_2d, lat_2d), method=method, fill_value=np.nan)
+        logic = np.isnan(data_2d0)
+        data_2d0[logic] = 0.0
+        return lon_2d, lat_2d, data_2d0
+
+
+
+def grid_by_dxdy(lon, lat, data, extent=None, dx=None, dy=None, method='nearest'):
+
+    """
+    !!!!!!!!! incomplete !!!!!!!!!
+    Grid irregular data into a regular grid by input 'extent' (westmost, eastmost, southmost, northmost)
+    Input:
+        lon: numpy array, input longitude to be gridded
+        lat: numpy array, input latitude to be gridded
+        data: numpy array, input data to be gridded
+        extent=: Python list, [westmost, eastmost, southmost, northmost]
+        NxNy=: Python list, [Nx, Ny], lon_2d = np.linspace(westmost, eastmost, Nx)
+                                      lat_2d = np.linspace(southmost, northmost, Ny)
+    Output:
+        lon_2d : numpy array, gridded longitude
+        lat_2d : numpy array, gridded latitude
+        data_2d: numpy array, gridded data
+    How to use:
+        After read in the longitude latitude and data into lon0, lat0, data0
+        lon, lat data = grid_by_extent(lon0, lat0, data0, extent=[10, 15, 10, 20])
+    """
+
+    # get central lon/lat
+    #/----------------------------------------------------------------------------\#
+    lon0 = np.nanmean(lon)
+    lat0 = np.nanmean(lat)
+    #\----------------------------------------------------------------------------/#
+
+
+    # get extent
+    #/----------------------------------------------------------------------------\#
+    if extent is None:
+        extent = [lon.min(), lon.max(), lat.min(), lat.max()]
+    #\----------------------------------------------------------------------------/#
+
+
+    # Nx and Ny
+    #/----------------------------------------------------------------------------\#
+    xy = (extent[1]-extent[0])*(extent[3]-extent[2])
+    N0 = np.sqrt(lon.size/xy)
+
+    Nx = int(N0*(extent[1]-extent[0]))
+    if Nx%2 == 1:
+        Nx += 1
+
+    Ny = int(N0*(extent[3]-extent[2]))
+    if Ny%2 == 1:
+        Ny += 1
+    #\----------------------------------------------------------------------------/#
+
+
+    if (dx is None) or (dy is None):
+
+        # calculate dx and dy
+        #/----------------------------------------------------------------------------\#
+        lon0 = extent[0]
+        lat0 = (extent[2]+extent[3])/2.0
+        lon1 = extent[1]
+        lat1 = (extent[2]+extent[3])/2.0
+        dx = cal_geodesic_dist(lon0, lat0, lon1, lat1)/Nx
+
+        lon0 = (extent[0]+extent[1])/2.0
+        lat0 = extent[2]
+        lon1 = (extent[0]+extent[1])/2.0
+        lat1 = extent[3]
+        dy = cal_geodesic_dist(lon0, lat0, lon1, lat1)/Ny
+        #\----------------------------------------------------------------------------/#
+
+    lon_1d0 = np.linspace(extent[0], extent[1], Nx+1)
+    lat_1d0 = np.linspace(extent[2], extent[3], Ny+1)
+
+    lon_1d = (lon_1d0[1:]+lon_1d0[:-1])/2.0
+    lat_1d = (lat_1d0[1:]+lat_1d0[:-1])/2.0
+
+    lat_2d, lon_2d = np.meshgrid(lat_1d, lon_1d)
+
+    points   = np.transpose(np.vstack((lon, lat)))
+    data_2d0 = interpolate.griddata(points, data, (lon_2d, lat_2d), method='linear', fill_value=np.nan)
+
+    if method == 'nearest':
+        data_2d  = interpolate.griddata(points, data, (lon_2d, lat_2d), method='nearest')
+        logic = np.isnan(data_2d0) | np.isnan(data_2d)
+        data_2d[logic] = 0.0
+        return lon_2d, lat_2d, data_2d
+    else:
         logic = np.isnan(data_2d0)
         data_2d0[logic] = 0.0
         return lon_2d, lat_2d, data_2d0
@@ -1091,6 +1181,7 @@ def download_lance_https(
                 warnings.warn(msg)
 
     return fnames_local
+
 
 
 def download_worldview_rgb(
