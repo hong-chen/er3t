29aa932afc48ac39cdc59a95c36018ec38dbd679
Yu-Wen Chen <r05223131@ntu.edu.tw>
2023-06-20 17:45:44 -0600

    YWC modified

--- er3t/pre/abs/abs_oco.py
+++ er3t/pre/abs/abs_oco.py
@@ -13,7 +13,7 @@ import er3t
 
 
 
-__all__ = ['abs_oco', 'abs_oco_idl']
+__all__ = ['abs_oco', 'abs_oco_idl', 'abs_oco_h5']
 
 
 
@@ -311,6 +311,142 @@ class abs_oco_idl:
                      }
 
 
+class abs_oco_h5:
+
+    """
+    Added by Yu-Wen Chen (Yu-Wen.Chen@colorado.edu)
+    Date: 2023.06.20
+
+    This part is modified from abs_oco_idl but read a self-defined absorption coefficient output.
+
+    Input:
+        wavelength: wavelength in nm
+        fname     : file path for the correlated-k coefficients (in pickle format)
+        atm_obj   : atmosphere object, e.g., atm_obj = atm_atmmod(levels=np.arange(21))
+
+    Note:
+        If wavelength is provided but fname does not exisit:
+            calculate correlated-k coefficients and save data into fname
+
+        If wavelength is provided but fname is None:
+            calculate correlated-k coefficients without saving the data
+
+        if wavelength is not provided but fname is provided (also existed):
+            read out the data from fname
+
+        if wavelength and fname are neither provided:
+            exit with error message
+
+    Output:
+        self.coef['wvl']
+        self.coef['abso_coef']
+        self.coef['slit_func']
+        self.coef['solar']
+        self.coef['weight']
+    """
+
+    def __init__(self, \
+                 wavelength  = None,  \
+                 fname       = None,  \
+                 fname_h5   = None,  \
+                 atm_obj     = None,  \
+                 overwrite   = False, \
+                 verbose     = False):
+
+        if fname_h5 is None:
+            self.fname_h5 = '%s/abs/abs_oco_11.h5' % er3t.common.fdir_data
+        else:
+            self.fname_h5 = fname_h5
+
+        self.verbose   = verbose
+        self.wvl       = wavelength
+
+        if ((fname is not None) and (os.path.exists(fname)) and (not overwrite)):
+
+            self.load(fname)
+
+        elif ((wavelength is not None) and (fname is not None) and (os.path.exists(fname)) and (overwrite)) or \
+             ((wavelength is not None) and (fname is not None) and (not os.path.exists(fname))):
+
+            self.run(atm_obj)
+            self.dump(fname)
+
+        elif ((wavelength is not None) and (fname is None)):
+
+            self.run(atm_obj)
+
+        else:
+
+            sys.exit('Error   [abs_oco_h5]: Please provide \'wavelength\' to proceed.')
+
+
+    def load(self, fname):
+
+        with open(fname, 'rb') as f:
+            obj = pickle.load(f)
+            if hasattr(obj, 'coef'):
+                if self.verbose:
+                    print('Message [abs_oco_h5]: Loading %s ...' % fname)
+                self.fname = obj.fname
+                self.wvl   = obj.wvl
+                self.nwl   = obj.nwl
+                self.coef  = obj.coef
+                self.Ng    = obj.Ng
+            else:
+                sys.exit('Error   [abs_oco_h5]: \'%s\' is not the correct pickle file to load.' % fname)
+
+
+    def run(self, atm_obj):
+
+        if not os.path.exists(self.fname_h5):
+            sys.exit('Error   [abs_oco_h5]: Missing IDL database.')
+
+        # self.coef
+        #     self.coef['wvl']
+        #     self.coef['abso_coef']
+        #     self.coef['slit_func']
+        #     self.coef['solar']
+        self.get_coefficient()
+
+
+    def dump(self, fname):
+
+        self.fname = fname
+        with open(fname, 'wb') as f:
+            if self.verbose:
+                print('Message [abs_oco_h5]: Saving object into %s ...' % fname)
+            pickle.dump(self, f)
+
+
+    def get_coefficient(self, wvl_threshold=1.0):
+        with h5py.File(self.fname_h5, 'r') as f:
+            wvl_center_oco = f['lamx'][...]*1000.0
+
+            index_wvl = np.argmin(np.abs(wvl_center_oco-self.wvl))
+            if abs(wvl_center_oco[index_wvl]-self.wvl) >= wvl_threshold:
+                sys.exit('Error   [abs_oco_h5]: Cannot pick a close wavelength for %.2fnm from \'%s\'.' % (self.wvl, self.fname_h5))
+            else:
+                if self.verbose:
+                    print('Message [abs_oco_h5]: Picked wvl=%.2f from \'%s\' for input wavelength %.2fnm.' % (wvl_center_oco[index_wvl], self.fname_h5, self.wvl))
+                self.wvl = wvl_center_oco[index_wvl]
+
+            Ng              = f['absgn'][...][index_wvl]
+            wvls            = f['absgx'][...][index_wvl, :Ng].T * 1000.0
+            abso_coef       = f['absgl'][...][:, index_wvl, :Ng].T
+            slit_func0      = f['absgy'][...][index_wvl, :Ng].T
+            slit_func       = np.empty(abso_coef.shape, dtype=slit_func0.dtype)
+            slit_func[...]  = slit_func0[:, None]
+            solar           = f['solx'][index_wvl, :Ng].T
+            weight          = slit_func0/slit_func0.sum()
+            
+            self.Ng         = Ng
+            self.nwl        = wvls.size
+            self.coef = {'wvl'       : {'name':'Wavelength'                     , 'data':self.wvl, 'units':'nm'},
+                        'abso_coef' : {'name':'Absorption Coefficient (Nz, Ng)', 'data':np.transpose(abso_coef)},
+                        'slit_func' : {'name':'Slit Function (Nz, Ng)'         , 'data':np.transpose(slit_func)},
+                        'solar'     : {'name':'Solar Factor (Ng)'              , 'data':solar},
+                        'weight'    : {'name':'Weight (Ng)'                    , 'data':weight}
+                        }
 
 
 if __name__ == '__main__':
