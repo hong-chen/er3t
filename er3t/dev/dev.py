480040a330d952623188170ccbf550ab0bbee5a7
hong-chen <hong.chen.cu@gmail.com>
2023-06-25 23:05:55 -0600

    implementing grid_nearest_fast

--- er3t/dev/dev.py
+++ er3t/dev/dev.py
@@ -16,140 +16,79 @@ import er3t
 
 
 
-__all__ = ['rearrange_jobs']
+__all__ = ['grid_nearest_fast']
 
 
-def rearrange_jobs(Ncpu, weights_in):
 
-    """
-    Purpose:
-        Rearrange jobs to optimize computational time by assigning specific set of jobs
-        to specific CPU so jobs from different CPUs can complete at a similar time length.
 
-    Input:
-        Ncpu: number of CPU
-        weights_in: input weights, e.g., photons_dist
+def grid_nearest_fast(x, y, data, x_2d, y_2d, fill_nan=True):
 
-    Output:
-        indices_out: optimized order for the input weights
-    """
-
-    # make input weights an array
+    # check equidistant
     #/----------------------------------------------------------------------------\#
-    weights_in = np.array(weights_in.ravel())
+    if check_equidistant(x_2d[:, 0]) and check_equidistant(x_2d[:, -1]) and
+       check_equidistant(y_2d[0, :]) and check_equidistant(y_2d[-1, :]):
+
+        dx = x_2d[1, 0] - x_2d[0, 0]
+        dy = y_2d[0, 1] - y_2d[0, 0]
+
+    else:
+
+        msg = '\nError [grid_nearest_fast]: Do not support non-equidistant gridding.'
+        raise ValueError(msg)
     #\----------------------------------------------------------------------------/#
 
 
-    # weights
+    # check whether raw data is contained within the gridded region
     #/----------------------------------------------------------------------------\#
-    weights = weights_in.copy()
-    weights += weights.min()
-    Nweight = weights.size
-    ids = np.arange(Nweight)
+    logic_in  = (x>=x_2d[0, 0]) & (x<=x_2d[-1, 0]) & (y>=y_2d[0, 0]) & (y<=y_2d[0, -1])
+    logic_out = np.logical_not(logic_in)
     #\----------------------------------------------------------------------------/#
 
 
-    # sort weights in descending order
+    # preprocess raw data
     #/----------------------------------------------------------------------------\#
-    indices = np.argsort(weights)[::-1]
-    ids     = ids[indices]
-    weights = weights[indices]
+    x = np.array(x).ravel()
+    y = np.array(y).ravel()
+    data = np.array(data).ravel()
     #\----------------------------------------------------------------------------/#
 
 
-    # make jobs (python list)
+    # define data_2d for storing gridded data
     #/----------------------------------------------------------------------------\#
-    jobs = list(zip(ids, weights))
-    Njob = len(jobs)
+    data_2d = np.zeros(x_2d.shape, dtype=np.float64)
+    if fill_nan:
+        data_2d[...] = np.nan
     #\----------------------------------------------------------------------------/#
 
 
-    # define workers (python dict)
+    indices_x = np.int_((x-x_2d[0, 0])//dx)
+    indices_y = np.int_((y-y_2d[0, 0])//dy)
+
+    nearest = np.zeros(x.size, dtype=np.float64)
+    nearest[logic_in] = data_2d[indices_x[logic_in], indices_y[logic_in]]
+
+    # deal with nan data
     #/----------------------------------------------------------------------------\#
-    workers = {iworker:[] for iworker in range(Ncpu)}
+    logic_nan  = np.isnan(data_2d)
+    logic_good = np.logical_not(logic_nan)
+    if np.isnan(nearest).sum()>0 and fill_nan:
+        data_2d_ = data_2d[logic_good]
+        x_2d_    = x_2d[logic_good]
+        y_2d_    = y_2d[logic_good]
+
+        indices_nan = np.where(np.isnan(nearest))[0]
+        for index in indices_nan:
+            x_ = x[index]
+            y_ = y[index]
+            index_closest = np.argmin(np.abs((x_2d_-x_)**2+(y_2d_-y_)**2))
+            nearest[index] = data_2d_[index_closest]
+
+    nearest[logic_out] = np.nan
     #\----------------------------------------------------------------------------/#
 
-
-    # assign jobs to workers in a way that the difference between the work load of
-    # workers is minimized
-    #/----------------------------------------------------------------------------\#
-    loads_now = np.zeros(Ncpu, dtype=np.float64)
-    while Njob > 0:
-
-        job0 = jobs[0]
-
-        id0  = job0[0]
-        weight0 = job0[1]
-
-        loads_diff = ((loads_now+weight0)-loads_now.min())**2
-        iplace = np.argmin(loads_diff)
-
-        loads_now[iplace] += weight0
-        workers[iplace].append(job0)
-
-        jobs.pop(0)
-        Njob = len(jobs)
-    #\----------------------------------------------------------------------------/#
+    return nearest
 
 
-    # sorted workers with number of jobs in descending order
-    #/----------------------------------------------------------------------------\#
-    rounds = [len(workers[i]) for i in range(Ncpu)]
-    indices = np.argsort(np.array(rounds))[::-1]
-    workers_sorted = {iworker:[] for iworker in range(Ncpu)}
-
-    for i in range(Ncpu):
-        workers_sorted[i] = workers[indices[i]]
-
-    # workers_sorted_ = copy.deepcopy(workers_sorted) # variable backup
-    #\----------------------------------------------------------------------------/#
-
-
-    # rearrange jobs
-    #/----------------------------------------------------------------------------\#
-    rounds = [len(workers_sorted[i]) for i in range(Ncpu)]
-    Nround = max(rounds)
-
-    indices_out = []
-    i_next_round = np.arange(Ncpu)
-
-    counts = np.zeros(Ncpu)
-
-    while Nround > 0:
-
-        weight0_round = np.array([], dtype=np.float64)
-        index0_round = np.array([], dtype=np.int32)
-
-        for i in i_next_round:
-
-            loads0 = workers_sorted[i]
-            if len(loads0) > 0:
-                id0 = loads0[0][0]
-                weight0 = loads0[0][1]
-
-                counts[i] += weight0
-
-                weight0_round = np.append(weight0_round, weight0)
-                index0_round  = np.append(index0_round, i)
-
-                indices_out.append(id0)
-
-                workers_sorted[i].pop(0)
-
-        if Nround == max(rounds):
-            weight0_base = weight0_round[:weight0_round.size].copy()
-        else:
-            weight0_base = weight0_base[:weight0_round.size].copy()
-            weight0_base += weight0_round
-
-        i_next_round = index0_round[np.argsort(weight0_base)]
-
-        Nround = max(len(workers_sorted[i]) for i in range(Ncpu))
-
-    indices_out = np.array(indices_out)
-    #\----------------------------------------------------------------------------/#
-
-    return indices_out
 
 
 if __name__ == '__main__':
