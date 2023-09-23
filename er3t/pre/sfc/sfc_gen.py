601f0f17c30f13922406c2e223f5c0a53327a1f7
Hong Chen <hong.chen.cu@gmail.com>
2023-09-23 10:07:47 -0600

    added references for ocean brdf

--- er3t/pre/sfc/sfc_gen.py
+++ er3t/pre/sfc/sfc_gen.py
@@ -4,7 +4,9 @@ import copy
 import pickle
 import numpy as np
 
-import er3t
+
+import er3t.common
+import er3t.util
 
 
 
@@ -51,8 +53,11 @@ def cal_ocean_brdf(
 
 
     # refractive index of water as a function of wavelength and salinity
-    # Hale and Querry, 1973: Optical Constants of Water in the 200-nm to 200-μm Wavelength Region
     #/----------------------------------------------------------------------------\#
+    reference = '\nRefractive Index of Water (Hale and Querry, 1973):
+- Hale, G. M., and Querry, M. R.: Optical Constants of Water in the 200-nm to 200-μm Wavelength Region, Appl. Opt. 12, 555-563, https://doi.org/10.1364/AO.12.000555, 1973.'
+    er3t.util.add_reference(reference)
+
     refractive_index_water = {
             'wvl': np.array([ \
                    0.250,0.275,0.300,0.325,0.345,0.375,0.400,0.425,0.445,0.475,\
@@ -92,10 +97,11 @@ def cal_ocean_brdf(
     refrac_i = np.interp(wvl, refractive_index_water['wvl'], refractive_index_water['imaginary'])
 
     # salinity corrections
-    #   - Friedman, 1969: Infrared Characteristics of Ocean Water (1.5-15μ)
-    #   - McLellan, 1965: Elements of Physical Oceanography
-    #   - Sverdrup et al., 1942 - The Oceans
     #/--------------------------------------------------------------\#
+    reference = '\nSalinity Correction (Friedman, 1969):
+- Friedman, D.: Infrared Characteristics of Ocean Water (1.5 –15 μ), Appl. Opt. 8, 2073-2078, https://doi.org/10.1364/AO.8.002073, 1969.'
+    er3t.util.add_reference(reference)
+
     refrac_r += 0.006*(sal/34.3)
     # refrac_i += 0.000*(sal/34.3)
     #\--------------------------------------------------------------/#
@@ -103,16 +109,23 @@ def cal_ocean_brdf(
 
 
     # variance of micro-scopic surface slope
-    #   - Cox and Munk, 1954: Measurement of the roughness of the sea surface from photographs of the sun’s glitter
     #/----------------------------------------------------------------------------\#
+    reference = '\nCox-Munk Parameterization (Cox and Munk, 1954):
+- Cox, C., and Munk, W.: Measurement of the Roughness of the Sea Surface from Photographs of the Sun’s Glitter, J. Opt. Soc. Am. 44, 838-850, https://doi.org/10.1364/JOSA.44.000838, 1954.'
+    er3t.util.add_reference(reference)
+
     slope = 0.00512*u10 + 0.003
     #\----------------------------------------------------------------------------/#
 
 
     # whitecaps treatment
-    #   - Koepke, 1984 - Effective reflectance of oceanic whitecaps
     #/----------------------------------------------------------------------------\#
     if whitecaps:
+
+        reference = '\nWhitecaps (Koepke, 1984):
+- Koepke, P.: Effective reflectance of oceanic whitecaps, Appl. Opt. 23, 1816-1824, https://doi.org/10.1364/AO.23.001816, 1984.'
+        er3t.util.add_reference(reference)
+
         reflectance_whitecaps = {
                 'wvl': np.arange(200.0, 4001.0, 100.0),
                 'ref': np.array([
@@ -125,7 +138,9 @@ def cal_ocean_brdf(
 
         diffuse_frac = 2.95e-06 * (u10**3.52)
         diffuse_alb  = np.interp(wvl, reflectance_whitecaps['wvl'], reflectance_whitecaps['ref'])
+
     else:
+
         diffuse_frac = 0.0*u10
         diffuse_alb  = 0.0*u10
     #\----------------------------------------------------------------------------/#
