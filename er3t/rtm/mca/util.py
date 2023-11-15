4ac83c0090d6e4e7ef30e87c5676db1042678fa8
Ken Hirata <ken.hirata@colorado.edu>
2023-11-14 17:38:05 -0700

    Cloud effective radius implemetation initial

--- er3t/rtm/mca/util.py
+++ er3t/rtm/mca/util.py
@@ -3,7 +3,7 @@ import sys
 import glob
 import datetime
 import numpy as np
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, RegularGridInterpolator, griddata
 import h5py
 
 import er3t
@@ -11,6 +11,7 @@ import er3t
 
 __all__ = [
         'func_ref_vs_cot',
+        'func_ref_vs_cot_cer',
         ]
 
 
@@ -202,6 +203,216 @@ class func_ref_vs_cot:
 
         return f(cot)
 
+
+class func_ref_vs_cot_cer:
+
+    def __init__(self,
+            cot,
+            cer,
+            fdir=er3t.common.params['fdir_tmp'],
+            date=er3t.common.params['date'],
+            wavelength1=650.0,
+            wavelength2=1640.0,
+            surface_albedo1=er3t.common.params['surface_albedo'],
+            surface_albedo2=er3t.common.params['surface_albedo'],
+            atmospheric_profile=er3t.common.params['atmospheric_profile'],
+            solar_zenith_angle=er3t.common.params['solar_zenith_angle'],
+            solar_azimuth_angle=er3t.common.params['solar_azimuth_angle'],
+            sensor_zenith_angle=er3t.common.params['sensor_zenith_angle'],
+            sensor_azimuth_angle=er3t.common.params['sensor_azimuth_angle'],
+            sensor_altitude=er3t.common.params['sensor_altitude'],
+            cloud_top_height=2.0,
+            cloud_geometrical_thickness=1.0,
+            solver='3d',
+            Nphoton=er3t.common.params['Nphoton'],
+            Ncpu=er3t.common.params['Ncpu'],
+            output_tag=er3t.common.params['output_tag'],
+            overwrite=er3t.common.params['overwrite'],
+            ):
+
+
+        self.cot  = cot
+        self.cer = cer
+        self.wvl1 = wavelength1
+        self.wvl2 = wavelength2
+        self.sza0 = solar_zenith_angle
+        self.saa0 = solar_azimuth_angle
+        self.vza0 = sensor_zenith_angle
+        self.vaa0 = sensor_azimuth_angle
+        self.alt0 = sensor_altitude
+        self.cth0 = cloud_top_height
+        self.cbh0 = cloud_top_height-cloud_geometrical_thickness
+        self.alb1 = surface_albedo1
+        self.alb2 = surface_albedo2
+        self.fdir = fdir
+        self.output_tag = output_tag
+        self.photon0 = Nphoton
+        self.solver0 = solver
+        self.cpu0 = Ncpu
+        self.date0 = date
+        self.fname_atm = atmospheric_profile
+
+        self.mu0  = np.cos(np.deg2rad(self.sza0))
+
+        if not overwrite:
+            try:
+                self.load_all()
+            except:
+                self.run_all()
+                self.load_all()
+        else:
+            self.run_all()
+            self.load_all()
+
+    def load_all(self):
+
+        self.rad     = np.zeros((self.cot.size, self.cer.size, 2))
+        self.rad_std = np.zeros((self.cot.size, self.cer.size, 2))
+        for i in range(self.cot.size):
+            for j in range(self.cer.size):
+                name_tag1 = 'cot-%05.1f_cer-%04.1f_wvl-%06.1f' % (self.cot[i], self.cer[j], self.wvl1)
+                name_tag2 = 'cot-%05.1f_cer-%04.1f_wvl-%06.1f' % (self.cot[i], self.cer[j], self.wvl2)
+
+                # read data
+                #/----------------------------------------------------------------------------\#
+                fname = '%s/%s_%s.h5' % (self.fdir, self.output_tag, name_tag1)
+                f0 = h5py.File(fname, 'r')
+                rad0     = f0['mean/rad'][...].mean()
+                rad_std0 = f0['mean/rad_std'][...].mean()
+                toa0     = f0['mean/toa'][...]
+                f0.close()
+                #\----------------------------------------------------------------------------/#
+                self.rad[i, j, 0]     = rad0
+                self.rad_std[i, j, 0] = rad_std0
+                #/----------------------------------------------------------------------------\#
+                fname = '%s/%s_%s.h5' % (self.fdir, self.output_tag, name_tag2)
+                f0 = h5py.File(fname, 'r')
+                rad0     = f0['mean/rad'][...].mean()
+                rad_std0 = f0['mean/rad_std'][...].mean()
+                toa0     = f0['mean/toa'][...]
+                f0.close()
+                #\----------------------------------------------------------------------------/#
+                self.rad[i, j, 1]     = rad0
+                self.rad_std[i, j, 1] = rad_std0
+
+        # convert from rad to ref
+        #/----------------------------------------------------------------------------\#
+        self.toa0    = toa0
+        self.ref     = np.pi*self.rad      / (toa0*self.mu0)
+        self.ref_std = np.pi*self.rad_std  / (toa0*self.mu0)
+        #\----------------------------------------------------------------------------/#
+
+    def run_all(self):
+
+        os.system('rm -rf %s' % self.fdir)
+        os.makedirs(self.fdir)
+
+        for cot0 in self.cot:
+            for cer0 in self.cer:
+                self.run_one(cot0, cer0, self.alb1, self.wvl1, cbh0=self.cbh0, cth0=self.cth0)
+                self.run_one(cot0, cer0, self.alb1, self.wvl2, cbh0=self.cbh0, cth0=self.cth0)
+
+    def run_one(self, cot0, cer0, alb, wvl, cbh0=1.0, cth0=2.0):
+
+        name_tag = 'cot-%05.1f_cer-%04.1f_wvl-%06.1f' % (cot0, cer0, wvl)
+
+        # atm object
+        #/----------------------------------------------------------------------------\#
+        levels = np.arange(0.0, 20.1, 0.1)
+        fname_atm = '%s/atm_wvl-%06.1fnm.pk' % (self.fdir, wvl)
+        atm0   = er3t.pre.atm.atm_atmmod(levels=levels, fname=fname_atm, fname_atmmod=self.fname_atm, overwrite=False)
+        #\----------------------------------------------------------------------------/#
+
+        # abs object
+        #/----------------------------------------------------------------------------\#
+        fname_abs = '%s/abs_wvl-%06.1fnm.pk' % (self.fdir, wvl)
+        abs0      = er3t.pre.abs.abs_16g(wavelength=wvl, fname=fname_abs, atm_obj=atm0, overwrite=False)
+        #\----------------------------------------------------------------------------/#
+
+        # phase function
+        #/----------------------------------------------------------------------------\#
+        pha0 = er3t.pre.pha.pha_mie_wc(wavelength=wvl, overwrite=False)
+        #\----------------------------------------------------------------------------/#
+
+        # mca_sca object
+        #/----------------------------------------------------------------------------\#
+        fname_sca = '%s/mca_sca-%06.1fnm.bin' % (self.fdir, wvl)
+        sca0  = er3t.rtm.mca.mca_sca(pha_obj=pha0, fname=fname_sca, overwrite=True)
+        #\----------------------------------------------------------------------------/#
+
+        # cloud setup
+        #/----------------------------------------------------------------------------\#
+        iref = np.argmin(np.abs(pha0.data['ref']['data']-cer0))
+        ext0 = cot0/(cth0-cbh0)/1000.0
+        ssa0 = pha0.data['ssa']['data'][iref]
+        asy0 = iref + 1
+        #\----------------------------------------------------------------------------/#
+
+        # mca_cld object
+        #/----------------------------------------------------------------------------\#
+        atm1d0  = er3t.rtm.mca.mca_atm_1d(atm_obj=atm0, abs_obj=abs0)
+        atm1d0.add_mca_1d_atm(ext1d=ext0, omg1d=ssa0, apf1d=asy0, z_bottom=cbh0, z_top=cth0)
+
+        atm_1ds   = [atm1d0]
+        atm_3ds   = []
+        #\----------------------------------------------------------------------------/#
+
+        # run mcarats
+        #/----------------------------------------------------------------------------\#
+        mca0 = er3t.rtm.mca.mcarats_ng(
+                date=self.date0,
+                atm_1ds=atm_1ds,
+                atm_3ds=atm_3ds,
+                sca=sca0,
+                target='radiance',
+                surface_albedo       = alb,
+                solar_zenith_angle   = self.sza0,
+                solar_azimuth_angle  = self.saa0,
+                sensor_zenith_angle  = self.vza0,
+                sensor_azimuth_angle = self.vaa0,
+                sensor_altitude      = self.alt0,
+                fdir='%s/%s_%s/rad' % (self.fdir, self.output_tag, name_tag),
+                Nrun=3,
+                Ng=abs0.Ng,
+                weights=abs0.coef['weight']['data'],
+                photons=self.photon0,
+                solver=self.solver0,
+                Ncpu=self.cpu0,
+                mp_mode='py',
+                overwrite=True
+                )
+        #\----------------------------------------------------------------------------/#
+
+
+        # mcarats output
+        #/----------------------------------------------------------------------------\#
+        out0 = er3t.rtm.mca.mca_out_ng(fname='%s/%s_%s.h5' % (self.fdir, self.output_tag, name_tag), mca_obj=mca0, abs_obj=abs0, mode='mean', squeeze=True, verbose=True, overwrite=True)
+        #\----------------------------------------------------------------------------/#
+
+    def get_cot_cer_from_ref(self, ref1, ref2, method='cubic', mode='rt'):
+
+        if mode == '2s':
+            msg = '\n[get_cot_cer_from_ref] mode = 2s is no longer used.'
+            raise ValueError(msg)
+        elif mode == 'rt':
+            points = self.ref[:, :, :].reshape(self.cot.size*self.cer.size, 2)
+            cots   = np.tile(self.cot, (len(self.cer), 1))
+            cers   = np.tile(self.cer, (len(self.cot), 1)).T
+            cot_out = griddata(points, cots, (ref1, ref2), method='cubic')
+            cer_out = griddata(points, cers, (ref1, ref2), method='cubic')
+            
+            return cot_out, cer_out
+    
+    def get_ref_from_cot_cer(self, cot, cer, wvl, method='cubic', mode='rt'):
+
+        if mode == '2s':
+            msg = '\n[get_cot_from_ref] mode = 2s is no longer used.'
+            raise ValueError(msg)
+        elif mode == 'rt':
+            f1 = RegularGridInterpolator((self.cot, self.cer), self.ref[:, :, 0], kind=method, bounds_error=False)
+            f2 = RegularGridInterpolator((self.cot, self.cer), self.ref[:, :, 0], kind=method, bounds_error=False)
+
+        return f1(cot, cer), f2(cot, cer)
 
 
 
