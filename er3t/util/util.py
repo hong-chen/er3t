97c84b3828b5825e15d1d6035f433b14b75e3e3c
Vikas Nataraja <hnvikas14@gmail.com>
2024-06-29 16:29:54 -0600

    add time option and change map to starmap

--- er3t/util/util.py
+++ er3t/util/util.py
@@ -1101,6 +1101,23 @@ def region_parser(extent, lons, lats, geojson_fpath):
         llons, llats = parse_geojson(geojson_fpath)
         return llons, llats
 
+
+def format_time(total_seconds):
+    """
+    Convert seconds to hours, minutes, seconds, and milliseconds.
+
+    Parameters:
+    - total_seconds: The total number of seconds to convert.
+
+    Returns:
+    - A tuple containing hours, minutes, seconds, and milliseconds.
+    """
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+    milliseconds = (total_seconds - int(total_seconds)) * 1000
+
+    return (int(hours), int(minutes), int(seconds), milliseconds)
 #\---------------------------------------------------------------------------/
 
 if __name__ == '__main__':
