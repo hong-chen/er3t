6755d58c1a114bcb5250c232030fd6b2bd27f46f
Hong Chen <hong.chen.cu@gmail.com>
2025-03-05 21:59:52 -0700

    attempting to get docs to work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('assets'))
-sys.path.insert(0, os.path.abspath('source'))
+sys.path.insert(0, os.path.abspath('../assets'))
 
 
 # -- Project information -----------------------------------------------------
