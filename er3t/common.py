a27d43320f8cf8c333dcb4eebd46b82bbd57a266
Hong Chen <hong.chen.cu@gmail.com>
2025-05-01 17:04:27 -0600

    updated reference

--- er3t/common.py
+++ er3t/common.py
@@ -57,7 +57,7 @@ params = {
         }
 
 references = [
-'EaR³T (Chen et al., 2023):\n- Chen, H., Schmidt, K. S., Massie, S. T., Nataraja, V., Norgren, M. S., Gristey, J. J., Feingold, G., Holz, R. E., and Iwabuchi, H.: The Education and Research 3D Radiative Transfer Toolbox (EaR³T) - Towards the Mitigation of 3D Bias in Airborne and Spaceborne Passive Imagery Cloud Retrievals, Atmos. Meas. Tech., 16, 1971–2000, https://doi.org/10.5194/amt-16-1971-2023, 2023.\n- Chen, Y.-W., Schmidt, K. S., Chen, H., Massie, S. T., Kulawik, S. S., and Iwabuchi, H.: Mitigation of satellite OCO-2 CO₂ biases in the vicinity of clouds with 3D calculations using the Education and Research 3D Radiative Transfer Toolbox (EaR³T), Atmos. Meas. Tech., 18, 1859–1884, https://doi.org/10.5194/amt-18-1859-2025, 2025.'
+'EaR³T (Chen et al., 2023; Chen et al., 2025):\n- Chen, H., Schmidt, K. S., Massie, S. T., Nataraja, V., Norgren, M. S., Gristey, J. J., Feingold, G., Holz, R. E., and Iwabuchi, H.: The Education and Research 3D Radiative Transfer Toolbox (EaR³T) - Towards the Mitigation of 3D Bias in Airborne and Spaceborne Passive Imagery Cloud Retrievals, Atmos. Meas. Tech., 16, 1971–2000, https://doi.org/10.5194/amt-16-1971-2023, 2023.\n- Chen, Y.-W., Schmidt, K. S., Chen, H., Massie, S. T., Kulawik, S. S., and Iwabuchi, H.: Mitigation of satellite OCO-2 CO₂ biases in the vicinity of clouds with 3D calculations using the Education and Research 3D Radiative Transfer Toolbox (EaR³T), Atmos. Meas. Tech., 18, 1859–1884, https://doi.org/10.5194/amt-18-1859-2025, 2025.'
         ]
 
 _today_dt    = datetime.datetime.now(datetime.timezone.utc)
