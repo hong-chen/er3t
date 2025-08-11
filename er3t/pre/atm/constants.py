0ac336e01e761a714fe7ab04a9c3be0e58e0bafe
Vikas Nataraja <hnvikas14@gmail.com>
2025-08-11 15:48:00 -0600

    add constants file

---
+++ er3t/pre/atm/constants.py
@@ -0,0 +1,10 @@
+"""
+Define constants used commonly
+"""
+
+Na = 6.02214179e23  # Avogadro's number in mol^-1
+kb = 1.380649e-23  # Boltzmann constant in J/K
+R = 8.314472  # Ideal gas constant in J/(mol*K)
+Rd = 287.052874 # J/(kg*K), specific gas constant for dry air
+Rv = 461.525 # J/(kg*K), specific gas constant for water vapor
+EPSILON = Rd/Rv # ratio of the gas constant for dry air to that for water vapor (Rd/Rv) = 0.622 approx.
