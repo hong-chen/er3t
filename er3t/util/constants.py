b13f786be425aa41c02afd7bddb59b3969276b93
Vikas Nataraja <hnvikas14@gmail.com>
2025-08-25 14:38:21 -0600

    add molar mass of gases

--- er3t/util/constants.py
+++ er3t/util/constants.py
@@ -10,3 +10,12 @@ Rv = 461.525 # J/(kg*K), specific gas constant for water vapor
 EPSILON = Rd/Rv # ratio of the gas constant for dry air to that for water vapor (Rd/Rv) = 0.622 approx.
 g = 9.80665 # m/s^2, acceleration due to gravity
 M_dry = 0.02897  # kg/mol, molar mass of dry air
+
+# molar masses in g/mol of common atmospheric gases
+molar_masses = {'co2': 44.0095,
+                'ch4': 16.0425,
+                'o3': 47.9982,
+                'no2': 46.0055,
+                'so2': 64.0638,
+                'co': 28.0101,
+                'h2o': 18.01528}
