2b7babf72f1711b0bb0733625c90e73f8a2847fe
Hong Chen <hong.chen.cu@gmail.com>
2025-10-10 14:49:44 -0600

    added verbose tag

--- er3t/rtm/shd/shd_inp.py
+++ er3t/rtm/shd/shd_inp.py
@@ -193,6 +193,7 @@ def load_shd_inp_nml():
              ('ADAPT_GRID_FACTOR', 2.2),
              ('NUM_SH_TERM_FACTOR', 0.6),
              ('CELL_TO_POINT_RATIO', 1.5),
+             ('VERBOSE', 7),
 
              ('_footer', '$END'),
             ])
